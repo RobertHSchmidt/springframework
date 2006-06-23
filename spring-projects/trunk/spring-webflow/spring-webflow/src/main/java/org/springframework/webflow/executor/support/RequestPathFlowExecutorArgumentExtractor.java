722e1308e3129a8f53911f5bc1b9b4d4dbee95a6
klr8 <klr8@2e8e966d-6415-0410-94b9-dc4e65435d03>
2006-06-23 15:04:23 +0000

    Code review. Removed the appendFlowInputAttributesToRequestPath of RequestPathFlowExecutorArgumentExtractor.

--- spring-projects/trunk/spring-webflow/spring-webflow/src/main/java/org/springframework/webflow/executor/support/RequestPathFlowExecutorArgumentExtractor.java
+++ spring-projects/trunk/spring-webflow/spring-webflow/src/main/java/org/springframework/webflow/executor/support/RequestPathFlowExecutorArgumentExtractor.java
@@ -1,7 +1,19 @@
+/*
+ * Copyright 2002-2006 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.springframework.webflow.executor.support;
-
-import java.util.Iterator;
-import java.util.Map;
 
 import org.springframework.util.StringUtils;
 import org.springframework.web.util.WebUtils;
@@ -19,8 +31,7 @@ import org.springframework.webflow.support.FlowRedirect;
  * For example, the url
  * <code>http://localhost/springair/reservation/booking</code> would launch a
  * new execution of the <code>booking</code> flow, assuming a context path of
- * <code>/springair</code> and a servlet mapping of
- * <code>/reservation/*</code>.
+ * <code>/springair</code> and a servlet mapping of <code>/reservation/*</code>.
  * <p>
  * Note: this implementation only works with <code>ExternalContext</code>
  * implementations that return a valid
@@ -32,49 +43,6 @@ import org.springframework.webflow.support.FlowRedirect;
 public class RequestPathFlowExecutorArgumentExtractor extends FlowExecutorArgumentExtractor {
 
 	private static final char PATH_SEPARATOR_CHARACTER = '/';
-
-	/**
-	 * Flag indicating if flow input attributes should be appended to the
-	 * request path on a flow redirect request, instead of being appended as
-	 * standard URL query parameters.
-	 */
-	private boolean appendFlowInputAttributesToRequestPath = false;
-
-	/**
-	 * Returns the flag indicating if flow input attributes should be appended
-	 * to the request path to build a flow redirect request, instead of being
-	 * appended as standard URL query parameters.
-	 */
-	public boolean isAppendFlowInputAttributesToRequestPath() {
-		return appendFlowInputAttributesToRequestPath;
-	}
-
-	/**
-	 * Sets a flag indicating if flow input attributes should be appended to the
-	 * request path to build a flow redirect request, instead of being appended
-	 * as standard URL query parameters.
-	 * 
-	 * For example:
-	 * <ul>
-	 * <li>With request path appending turned on:
-	 * 
-	 * <pre>
-	 *        /booking/12345
-	 * </pre>
-	 * 
-	 * <li>With request path appending turned off:
-	 * 
-	 * <pre>
-	 *        /booking?bookingId=12345
-	 * </pre>
-	 * 
-	 * </ul>
-	 * 
-	 * @param appendFlowInputAttributesToRequestPath the boolean flag value
-	 */
-	public void setAppendFlowInputAttributesToRequestPath(boolean appendFlowInputAttributesToRequestPath) {
-		this.appendFlowInputAttributesToRequestPath = appendFlowInputAttributesToRequestPath;
-	}
 
 	public boolean isFlowIdPresent(ExternalContext context) {
 		String requestPathInfo = getRequestPathInfo(context);
@@ -94,29 +62,11 @@ public class RequestPathFlowExecutorArgumentExtractor extends FlowExecutorArgume
 		flowUrl.append(context.getDispatcherPath());
 		flowUrl.append(PATH_SEPARATOR_CHARACTER);
 		flowUrl.append(flowRedirect.getFlowId());
-		if (appendFlowInputAttributesToRequestPath) {
-			appendRequestPathElements(flowRedirect.getInput(), flowUrl);
-		}
-		else {
-			if (!flowRedirect.getInput().isEmpty()) {
-				flowUrl.append('?');
-				appendQueryParameters(flowRedirect.getInput(), flowUrl);
-			}
+		if (!flowRedirect.getInput().isEmpty()) {
+			flowUrl.append('?');
+			appendQueryParameters(flowRedirect.getInput(), flowUrl);
 		}
 		return flowUrl.toString();
-	}
-
-	private void appendRequestPathElements(Map map, StringBuffer url) {
-		if (!map.isEmpty()) {
-			url.append(PATH_SEPARATOR_CHARACTER);
-			Iterator it = map.values().iterator();
-			while (it.hasNext()) {
-				url.append(encodeValue(it.next()));
-				if (it.hasNext()) {
-					url.append(PATH_SEPARATOR_CHARACTER);
-				}
-			}
-		}
 	}
 
 	public String createFlowExecutionUrl(String flowExecutionKey, FlowExecutionContext flowExecution,
@@ -130,7 +80,13 @@ public class RequestPathFlowExecutorArgumentExtractor extends FlowExecutorArgume
 		appendQueryParameter(getFlowExecutionKeyParameterName(), flowExecutionKey, flowExecutionUrl);
 		return flowExecutionUrl.toString();
 	}
+	
+	// internal helpers
 
+	/**
+	 * Returns the request path info for given external context.
+	 * Never returns null, an empty string is returned instead.
+	 */
 	private String getRequestPathInfo(ExternalContext context) {
 		String requestPathInfo = context.getRequestPathInfo();
 		return requestPathInfo != null ? requestPathInfo : "";
