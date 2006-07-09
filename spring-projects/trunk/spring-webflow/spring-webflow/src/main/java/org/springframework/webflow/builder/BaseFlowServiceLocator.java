ca3e95f13f81cbf70783f006d8d21fc9e99ef417
kdonald <kdonald@2e8e966d-6415-0410-94b9-dc4e65435d03>
2006-07-09 18:10:21 +0000

    removed requirement that isAction actually reference a valid bean.

--- spring-projects/trunk/spring-webflow/spring-webflow/src/main/java/org/springframework/webflow/builder/BaseFlowServiceLocator.java
+++ spring-projects/trunk/spring-webflow/spring-webflow/src/main/java/org/springframework/webflow/builder/BaseFlowServiceLocator.java
@@ -127,8 +127,12 @@ public class BaseFlowServiceLocator implements FlowServiceLocator {
 		return (Action)getBean(id, Action.class);
 	}
 
-	public boolean isAction(String id) throws FlowArtifactLookupException {
-		return Action.class.isAssignableFrom(getBeanType(id, Action.class));
+	public boolean isAction(String id) {
+		if (containsBean(id)) {
+			return Action.class.isAssignableFrom(getBeanType(id, Action.class));
+		} else {
+			return false;
+		}
 	}
 
 	public FlowAttributeMapper getAttributeMapper(String id) throws FlowArtifactLookupException {
