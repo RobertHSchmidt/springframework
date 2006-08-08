82c24d629ff93fe3bb04d5231ef0d4f6fcebce18
kdonald <kdonald@2e8e966d-6415-0410-94b9-dc4e65435d03>
2006-08-08 05:44:41 +0000

    code restructuring on SonarJ analysis.

---
+++ spring-projects/trunk/spring-webflow/spring-webflow/src/main/java/org/springframework/webflow/definition/FlowDefinitionConstructionException.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2002-2006 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.webflow.definition;
+
+import org.springframework.webflow.FlowException;
+
+/**
+ * Thrown when a flow definition was found during a lookup operation
+ * but could not be constructed.
+ * 
+ * @author Keith Donald
+ * @author Erwin Vervaet
+ */
+public abstract class FlowDefinitionConstructionException extends FlowException {
+	
+	private String flowId;
+	
+	/**
+	 * Creates an exception indicating a flow definition could not be found.
+	 * @param flowId the flow id
+	 * @param cause underlying cause of the exception
+	 */
+	public FlowDefinitionConstructionException(String flowId, Throwable cause) {
+		super("An exception occured constructing the flow with id '" + flowId + "'", cause);
+	}
+	
+	public String getFlowId() {
+		return flowId;
+	}
+}
