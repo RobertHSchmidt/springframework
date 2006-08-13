aaf0d9b829f5d86c90e1a68ee7c58cc735723980
kdonald <kdonald@2e8e966d-6415-0410-94b9-dc4e65435d03>
2006-08-13 14:24:08 +0000

    javadoc

--- spring-projects/trunk/spring-webflow/spring-webflow/src/main/java/org/springframework/webflow/execution/factory/FlowExecutionFactory.java
+++ spring-projects/trunk/spring-webflow/spring-webflow/src/main/java/org/springframework/webflow/execution/factory/FlowExecutionFactory.java
@@ -4,17 +4,21 @@ import org.springframework.webflow.definition.FlowDefinition;
 import org.springframework.webflow.execution.FlowExecution;
 
 /**
- * An abstract factory for creating flow exections.  A flow execution represents a 
- * runtime, top-level instance of a flow definition.
+ * An abstract factory for creating flow exections. A flow execution represents
+ * a runtime, top-level instance of a flow definition.
+ * <p>
+ * This factory provides encapsulation of the flow execution implementation
+ * type, as well as its construction and assembly process.
  * 
  * @author Keith Donald
  */
 public interface FlowExecutionFactory {
-	
+
 	/**
-	 * Create a new flow execution product for the definition.
+	 * Create a new flow execution product for the flow definition.
 	 * @param flowDefinition the flow definition
-	 * @return the new flow execution, fully initialized and awaiting to be started.
+	 * @return the new flow execution, fully initialized and awaiting to be
+	 * started.
 	 */
 	public FlowExecution createFlowExecution(FlowDefinition flowDefinition);
 }
