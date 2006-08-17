24c83bfec491c6ed548b2a94e76524a114101fe5
kdonald <kdonald@2e8e966d-6415-0410-94b9-dc4e65435d03>
2006-08-17 12:54:20 +0000

    polish

---
+++ spring-projects/trunk/spring-webflow/spring-webflow/src/test/java/org/springframework/webflow/core/WebFlowOgnlExpressionParserTests.java
@@ -0,0 +1,9 @@
+package org.springframework.webflow.core;
+
+import junit.framework.TestCase;
+
+public class WebFlowOgnlExpressionParserTests extends TestCase {
+	public void testTest() {
+		
+	}
+}
