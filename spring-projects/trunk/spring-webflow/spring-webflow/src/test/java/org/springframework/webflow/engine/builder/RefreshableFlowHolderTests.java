de385c4b2753d54c93bb378a64fada44519cd980
kdonald <kdonald@2e8e966d-6415-0410-94b9-dc4e65435d03>
2006-08-17 12:33:21 +0000

    polish

--- spring-projects/trunk/spring-webflow/spring-webflow/src/test/java/org/springframework/webflow/engine/builder/RefreshableFlowHolderTests.java
+++ spring-projects/trunk/spring-webflow/spring-webflow/src/test/java/org/springframework/webflow/engine/builder/RefreshableFlowHolderTests.java
@@ -6,14 +6,13 @@ import junit.framework.TestCase;
 
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
-import org.springframework.webflow.engine.builder.FlowAssembler;
-import org.springframework.webflow.engine.builder.RefreshableFlowDefinitionHolder;
+import org.springframework.webflow.definition.FlowDefinition;
 import org.springframework.webflow.engine.builder.xml.XmlFlowBuilder;
 
 public class RefreshableFlowHolderTests extends TestCase {
 
 	public void testNoRefreshOnNoChange() {
-		File parent = new File("src/test/java/org/springframework/webflow/registry");
+		File parent = new File("src/test/java/org/springframework/webflow/engine/builder/xml");
 		Resource location = new FileSystemResource(new File(parent, "flow.xml"));
 		XmlFlowBuilder flowBuilder = new XmlFlowBuilder(location);
 		FlowAssembler assembler = new FlowAssembler("flow", flowBuilder);
@@ -22,25 +21,14 @@ public class RefreshableFlowHolderTests extends TestCase {
 		assertSame(flowBuilder, holder.getFlowBuilder());
 		assertEquals(0, holder.getLastModified());
 		assertTrue(!holder.isAssembled());
-		holder.getFlowDefinition();
+		FlowDefinition flow1 = holder.getFlowDefinition();
 		assertTrue(holder.isAssembled());
 		long lastModified = holder.getLastModified();
 		assertTrue(lastModified != -1);
 		assertTrue(lastModified > 0);
-		holder.getFlowDefinition();
+		FlowDefinition flow2 = holder.getFlowDefinition();
+		assertEquals("flow", flow2.getId());
 		assertEquals(lastModified, holder.getLastModified());
-	}
-
-	public void testNoRefreshOnChange() {
-		File parent = new File("src/test/java/org/springframework/webflow/registry");
-		Resource location = new FileSystemResource(new File(parent, "flow.xml"));
-		XmlFlowBuilder flowBuilder = new XmlFlowBuilder(location);
-		FlowAssembler assembler = new FlowAssembler("flow", flowBuilder);
-		RefreshableFlowDefinitionHolder holder = new RefreshableFlowDefinitionHolder(assembler);
-		holder.getFlowDefinition();
-		assertTrue(holder.isAssembled());
-		long lastModified = holder.getLastModified();
-		holder.getFlowDefinition();
-		assertEquals(lastModified, holder.getLastModified());
+		assertSame(flow1, flow2);
 	}
 }
