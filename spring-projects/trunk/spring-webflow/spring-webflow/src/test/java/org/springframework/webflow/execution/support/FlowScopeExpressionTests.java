afb65fdd61eeb1ca87f6979e69d367bb1dd9c35a
kdonald <kdonald@2e8e966d-6415-0410-94b9-dc4e65435d03>
2006-08-17 16:30:10 +0000

    polish

--- spring-projects/trunk/spring-webflow/spring-webflow/src/test/java/org/springframework/webflow/execution/support/FlowScopeExpressionTests.java
+++ spring-projects/trunk/spring-webflow/spring-webflow/src/test/java/org/springframework/webflow/execution/support/FlowScopeExpressionTests.java
@@ -2,17 +2,20 @@ package org.springframework.webflow.execution.support;
 
 import junit.framework.TestCase;
 
-/**
- * Unit tests for the ContinuationFlowExecutionRepository class.
- * 
- * @author Ulrik Sandberg
- */
+import org.springframework.binding.expression.Expression;
+import org.springframework.webflow.core.DefaultExpressionParserFactory;
+import org.springframework.webflow.test.engine.MockRequestContext;
+
 public class FlowScopeExpressionTests extends TestCase {
 
 	protected void setUp() throws Exception {
 	}
 
-	public void testPlaceholder() {
-
+	public void testFlowScopeExpression() {
+		Expression exp = new DefaultExpressionParserFactory().getExpressionParser().parseExpression("foo");
+		FlowScopeExpression flowExp = new FlowScopeExpression(exp);
+		MockRequestContext context = new MockRequestContext();
+		context.getFlowScope().put("foo", "bar");
+		assertEquals("bar", flowExp.evaluateAgainst(context, null));
 	}
 }
