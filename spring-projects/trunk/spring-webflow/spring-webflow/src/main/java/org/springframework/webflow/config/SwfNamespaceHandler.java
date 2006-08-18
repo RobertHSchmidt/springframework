b18d8e0ec82f9bf30dae7c9b43be13d7599caacd
kdonald <kdonald@2e8e966d-6415-0410-94b9-dc4e65435d03>
2006-08-18 16:29:57 +0000

    2.0 schema

---
+++ spring-projects/trunk/spring-webflow/spring-webflow/src/main/java/org/springframework/webflow/config/SwfNamespaceHandler.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2002-2006 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.webflow.config;
+
+import org.springframework.beans.factory.xml.BeanDefinitionParser;
+import org.springframework.beans.factory.xml.NamespaceHandlerSupport;
+import org.springframework.webflow.executor.FlowExecutorImpl;
+
+/**
+ * <code>NamespaceHandler</code> for the <code>swf</code> namespace.
+ * 
+ * <p>
+ * Provides {@link BeanDefinitionParser}s for the <code>&lt;executor&gt;</code>
+ * and <code>&lt;registry&gt;</code> tags. A <code>executor</code> tag can
+ * include nested <code>listener</code> tags and a <code>registry</code> tag
+ * can include <code>location</code> tags.
+ * 
+ * Using the <code>executor</code> tag you can configure a
+ * {@link FlowExecutorImpl}. The <code>executor</code> tag allows you to
+ * specify the repository type, and a reference to a registry. The repository
+ * type enumeration will lead to creation of a
+ * {@link DefaultFlowExecutionRepositoryFactory}, a{@link ContinuationFlowExecutionRepositoryFactory},
+ * or a{@link ClientContinuationFlowExecutionRepositoryFactory}. The
+ * <code>executor</code> tag also supports references to listeners with
+ * in-line definition of listener criteria.
+ * 
+ * <pre class="code">
+ *     &lt;swf:executor id=&quot;registry&quot; repository=&quot;continuation&quot; registry-ref=&quot;registry&quot;&gt;
+ *         &lt;swf:listener ref=&quot;listener1&quot; /&gt;
+ *         &lt;swf:listener ref=&quot;listener2&quot; ref=&quot;*&quot; /&gt;
+ *         &lt;swf:listener ref=&quot;listener3&quot; ref=&quot;flow1, flow2, flow3&quot; /&gt;
+ *     &lt;/swf:executor&gt;
+ * </pre>
+ * 
+ * Using the <code>registry</code> tag you can configure an
+ * {@link org.springframework.webflow.engine.builder.registry.XmlFlowRegistryFactoryBean}
+ * for use by any number of <code>executor</code>s. The <code>registry</code>
+ * tag supports in-line flow definition locations.
+ * 
+ * <pre class="code">
+ *     &lt;swf:registry id=&quot;registry&quot;&gt;
+ *         &lt;swf:location path=&quot;/path/to/flow.xml&quot; /&gt;
+ *         &lt;swf:location path=&quot;/path/with/wildcards/*-flow.xml&quot; /&gt;
+ *     &lt;/swf:registry&gt;
+ * </pre>
+ * 
+ * @author Ben Hale
+ */
+public class SwfNamespaceHandler extends NamespaceHandlerSupport {
+	public void init() {
+		registerBeanDefinitionParser("executor", new ExecutorBeanDefinitionParser());
+		registerBeanDefinitionParser("registry", new RegistryBeanDefinitionParser());
+	}
+}
