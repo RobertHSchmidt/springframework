71d2c28b7cc7836bda50980143150d3dc16bf1aa
kdonald <kdonald@2e8e966d-6415-0410-94b9-dc4e65435d03>
2006-08-27 04:32:47 +0000

    docs

--- spring-projects/trunk/spring-webflow/spring-webflow/src/main/java/org/springframework/webflow/core/WebFlowOgnlExpressionParser.java
+++ spring-projects/trunk/spring-webflow/spring-webflow/src/main/java/org/springframework/webflow/core/WebFlowOgnlExpressionParser.java
@@ -22,7 +22,6 @@ import ognl.PropertyAccessor;
 
 import org.springframework.binding.expression.support.OgnlExpressionParser;
 import org.springframework.binding.util.MapAdaptable;
-import org.springframework.webflow.core.collection.LocalAttributeMap;
 import org.springframework.webflow.core.collection.MutableAttributeMap;
 
 /**
@@ -38,7 +37,7 @@ class WebFlowOgnlExpressionParser extends OgnlExpressionParser {
 	 */
 	public WebFlowOgnlExpressionParser() {
 		addPropertyAccessor(MapAdaptable.class, new MapAdaptablePropertyAccessor());
-		addPropertyAccessor(MutableAttributeMap.class, new AttributeMapPropertyAccessor());
+		addPropertyAccessor(MutableAttributeMap.class, new MutableAttributeMapPropertyAccessor());
 	}
 
 	/**
@@ -58,11 +57,11 @@ class WebFlowOgnlExpressionParser extends OgnlExpressionParser {
 	}
 
 	/**
-	 * The {@link LocalAttributeMap} property accessor.
+	 * The {@link MutableAttributeMap} property accessor.
 	 * 
 	 * @author Keith Donald
 	 */
-	private static class AttributeMapPropertyAccessor extends MapAdaptablePropertyAccessor {
+	private static class MutableAttributeMapPropertyAccessor extends MapAdaptablePropertyAccessor {
 		public void setProperty(Map context, Object target, Object name, Object value) throws OgnlException {
 			((MutableAttributeMap)target).put((String)name, value);
 		}
