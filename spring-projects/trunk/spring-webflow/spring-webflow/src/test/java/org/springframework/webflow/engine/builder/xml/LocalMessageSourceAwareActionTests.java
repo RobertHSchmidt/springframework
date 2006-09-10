52fc711ae8c40b581cc214564f76f7641b7839a6
kdonald <kdonald@2e8e966d-6415-0410-94b9-dc4e65435d03>
2006-09-10 02:29:59 +0000

    tested resolution of global messages from local aware action

---
+++ spring-projects/trunk/spring-webflow/spring-webflow/src/test/java/org/springframework/webflow/engine/builder/xml/LocalMessageSourceAwareActionTests.java
@@ -0,0 +1,7 @@
+package org.springframework.webflow.engine.builder.xml;
+
+import junit.framework.TestCase;
+
+public class LocalMessageSourceAwareActionTests extends TestCase {
+
+}
---
+++ spring-projects/trunk/spring-webflow/spring-webflow/src/test/java/org/springframework/webflow/engine/builder/xml/MessageSourceAwareTests.java
@@ -0,0 +1,45 @@
+package org.springframework.webflow.engine.builder.xml;
+
+import java.util.Locale;
+
+import junit.framework.TestCase;
+
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
+import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.context.support.StaticMessageSource;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.webflow.definition.registry.FlowDefinitionRegistryImpl;
+import org.springframework.webflow.engine.Flow;
+import org.springframework.webflow.engine.builder.DefaultFlowServiceLocator;
+import org.springframework.webflow.engine.builder.FlowAssembler;
+import org.springframework.webflow.engine.impl.FlowExecutionImplFactory;
+import org.springframework.webflow.execution.FlowExecution;
+import org.springframework.webflow.test.MockExternalContext;
+
+public class MessageSourceAwareTests extends TestCase {
+
+	private Flow flow;
+	
+	protected void setUp() throws Exception {
+		GenericApplicationContext context = new GenericApplicationContext();
+		BeanDefinitionBuilder builder = BeanDefinitionBuilder.rootBeanDefinition(FooMessageSource.class);
+		context.registerBeanDefinition("messageSource", builder.getBeanDefinition());
+		context.refresh();
+		DefaultFlowServiceLocator locator = new DefaultFlowServiceLocator(new FlowDefinitionRegistryImpl(), context);
+		XmlFlowBuilder flowBuilder = new XmlFlowBuilder(new ClassPathResource("messageSourceAwareFlow.xml", getClass()), locator);
+		flow = new FlowAssembler("flow", flowBuilder).assembleFlow();
+	}
+	
+	private static class FooMessageSource extends StaticMessageSource {
+		public FooMessageSource() {
+			addMessage("foo", Locale.getDefault(), "bar");
+		}		
+	}
+	
+	public void testAwareAction() {
+		FlowExecution execution = new FlowExecutionImplFactory().createFlowExecution(flow);
+		execution.start(null, new MockExternalContext());
+		assertFalse(execution.isActive());
+	}
+
+}
