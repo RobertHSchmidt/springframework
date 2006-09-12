5248c87d5f9ff945700b9805ddb2415602343902
kdonald <kdonald@2e8e966d-6415-0410-94b9-dc4e65435d03>
2006-09-12 18:25:18 +0000

    polish

--- spring-projects/trunk/spring-webflow/spring-binding/src/main/java/org/springframework/binding/convert/ConversionServiceAware.java
+++ spring-projects/trunk/spring-webflow/spring-binding/src/main/java/org/springframework/binding/convert/ConversionServiceAware.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2005 the original author or authors.
+ * Copyright 2002-2006 the original author or authors.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,7 +17,7 @@ package org.springframework.binding.convert;
 
 /**
  * Marker interface that denotes an object has a dependency on a conversion
- * service that is expected to be full-filled.
+ * service that is expected to be fulfilled.
  * @author Keith Donald
  */
 public interface ConversionServiceAware {
