86b5584a1b6f85110cbfceacfee8bf47456f8219
klr8 <klr8@2e8e966d-6415-0410-94b9-dc4e65435d03>
2006-09-19 19:05:59 +0000

    Made license statement consistent in all source files.

--- spring-projects/trunk/spring-webflow/spring-webflow/src/main/java/org/springframework/webflow/context/servlet/HttpSessionMapBindingListener.java
+++ spring-projects/trunk/spring-webflow/spring-webflow/src/main/java/org/springframework/webflow/context/servlet/HttpSessionMapBindingListener.java
@@ -1,6 +1,4 @@
 /*
- * Copyright 2005 the original author or authors.
- /*
  * Copyright 2002-2006 the original author or authors.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
