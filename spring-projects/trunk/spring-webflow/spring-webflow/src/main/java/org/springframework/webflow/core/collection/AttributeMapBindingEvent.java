b691f5826063c74753b12bdd0bc08efd00124d58
kdonald <kdonald@2e8e966d-6415-0410-94b9-dc4e65435d03>
2006-09-19 15:02:43 +0000

    moved to collection

--- spring-projects/trunk/spring-webflow/spring-webflow/src/main/java/org/springframework/webflow/core/collection/AttributeMapBindingEvent.java
+++ spring-projects/trunk/spring-webflow/spring-webflow/src/main/java/org/springframework/webflow/core/collection/AttributeMapBindingEvent.java
@@ -19,7 +19,7 @@ import java.util.EventObject;
 
 /**
  * Holder for information about the binding or unbinding event in an
- * <code>AttributeMap</code>.
+ * {@link AttributeMap}.
  * 
  * @see AttributeMapBindingListener
  * 
