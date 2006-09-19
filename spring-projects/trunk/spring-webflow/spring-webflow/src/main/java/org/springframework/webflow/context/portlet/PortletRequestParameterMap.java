2c0c077cbab5a708c7621f5d464392100991397b
kdonald <kdonald@2e8e966d-6415-0410-94b9-dc4e65435d03>
2006-09-19 14:27:08 +0000

    copyright

--- spring-projects/trunk/spring-webflow/spring-webflow/src/main/java/org/springframework/webflow/context/portlet/PortletRequestParameterMap.java
+++ spring-projects/trunk/spring-webflow/spring-webflow/src/main/java/org/springframework/webflow/context/portlet/PortletRequestParameterMap.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2005 the original author or authors.
+ * Copyright 2002-2006 the original author or authors.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -12,7 +12,7 @@
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
  * License for the specific language governing permissions and limitations under
  * the License.
- */
+ *//
 package org.springframework.webflow.context.portlet;
 
 import java.util.Iterator;
