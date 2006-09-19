05d9f463fbc13c9cacdab9d451603f914955f54c
klr8 <klr8@2e8e966d-6415-0410-94b9-dc4e65435d03>
2006-09-19 18:24:12 +0000

    Improved CompositeIterator and added unit test.

--- spring-projects/trunk/spring-webflow/spring-webflow/src/main/java/org/springframework/webflow/core/collection/CompositeIterator.java
+++ spring-projects/trunk/spring-webflow/spring-webflow/src/main/java/org/springframework/webflow/core/collection/CompositeIterator.java
@@ -5,14 +5,19 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.NoSuchElementException;
 
+import org.springframework.util.Assert;
+
 /**
- * Iterator that combines multiple other iterators
+ * Iterator that combines multiple other iterators. This is a simple implementation
+ * that just maintains a list of iterators which are invoked in sequence untill
+ * all iterators are exhausted.
  * 
  * @author Erwin Vervaet
  */
 public class CompositeIterator implements Iterator {
 
 	private List iterators = new LinkedList();
+	private boolean inUse = false;
 
 	/**
 	 * Create a new composite iterator. Add iterators using the {@link #add(Iterator)} method.
@@ -24,10 +29,15 @@ public class CompositeIterator implements Iterator {
 	 * Add given iterator to this composite.
 	 */
 	public void add(Iterator iterator) {
+		Assert.state(!inUse, "You can no longer add iterator to a composite iterator that's already in use");
+		if (iterators.contains(iterator)) {
+			throw new IllegalArgumentException("You cannot add the same iterator twice");
+		}
 		iterators.add(iterator);
 	}
 
 	public boolean hasNext() {
+		inUse = true;
 		for (Iterator it = iterators.iterator(); it.hasNext(); ) {
 			if (((Iterator)it.next()).hasNext()) {
 				return true;
@@ -37,6 +47,7 @@ public class CompositeIterator implements Iterator {
 	}
 
 	public Object next() {
+		inUse = true;
 		for (Iterator it = iterators.iterator(); it.hasNext(); ) {
 			Iterator iterator = (Iterator)it.next();
 			if (iterator.hasNext()) {
@@ -47,6 +58,6 @@ public class CompositeIterator implements Iterator {
 	}
 
 	public void remove() {
-		throw new UnsupportedOperationException("Remove not supported");
+		throw new UnsupportedOperationException("Remove is not supported");
 	}		
 }
