1e85bed0ab28759e3ed34558e75ecc509be4cb8b
poutsma <poutsma@2e8e966d-6415-0410-94b9-dc4e65435d03>
2006-09-23 22:57:53 +0000

    Fixed #SWS-53:  Missing contentId for methods 'addAttachment' in SoapMessage

--- spring-projects/trunk/spring-ws/spring-ws-core/src/main/java/org/springframework/ws/soap/saaj/SaajSoapMessage.java
+++ spring-projects/trunk/spring-ws/spring-ws-core/src/main/java/org/springframework/ws/soap/saaj/SaajSoapMessage.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2005 the original author or authors.
+ * Copyright 2005, 2006 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -188,6 +188,10 @@ public class SaajSoapMessage extends AbstractSoapMessage {
 
         public String getId() {
             return saajAttachment.getContentId();
+        }
+
+        public void setId(String id) {
+            saajAttachment.setContentId(id);
         }
 
         public String getContentType() {
