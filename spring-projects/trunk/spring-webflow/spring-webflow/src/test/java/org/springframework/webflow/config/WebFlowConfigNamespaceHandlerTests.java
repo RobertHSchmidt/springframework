39d48980bb3c607b53023e46a215f086267a4d3e
klr8 <klr8@2e8e966d-6415-0410-94b9-dc4e65435d03>
2006-09-28 20:58:20 +0000

    Improved unit tests for SWF config namespace.

--- spring-projects/trunk/spring-webflow/spring-webflow/src/test/java/org/springframework/webflow/config/WebFlowConfigNamespaceHandlerTests.java
+++ spring-projects/trunk/spring-webflow/spring-webflow/src/test/java/org/springframework/webflow/config/WebFlowConfigNamespaceHandlerTests.java
@@ -20,13 +20,23 @@ import junit.framework.TestCase;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.webflow.core.collection.AttributeMap;
 import org.springframework.webflow.definition.registry.FlowDefinitionRegistry;
-import org.springframework.webflow.executor.FlowExecutor;
+import org.springframework.webflow.engine.Flow;
+import org.springframework.webflow.engine.impl.FlowExecutionImplFactory;
+import org.springframework.webflow.engine.support.ApplicationViewSelector;
+import org.springframework.webflow.execution.factory.ConditionalFlowExecutionListenerLoader;
+import org.springframework.webflow.execution.factory.StaticFlowExecutionListenerLoader;
+import org.springframework.webflow.execution.repository.continuation.ClientContinuationFlowExecutionRepository;
+import org.springframework.webflow.execution.repository.continuation.ContinuationFlowExecutionRepository;
+import org.springframework.webflow.execution.repository.support.SimpleFlowExecutionRepository;
+import org.springframework.webflow.executor.FlowExecutorImpl;
 
 /**
- * Unit tests for the SwfNamespaceHandler and its BeanDefinitionParsers
+ * Unit tests for the WebFlowConfigNamespaceHandler and its BeanDefinitionParsers.
  * 
  * @author Ben Hale
+ * @author Erwin Vervaet
  */
 public class WebFlowConfigNamespaceHandlerTests extends TestCase {
 
@@ -54,8 +64,69 @@ public class WebFlowConfigNamespaceHandlerTests extends TestCase {
 		assertEquals("Incorrect number of flows loaded", 0, registry.getFlowDefinitionCount());
 	}
 
-	public void testExecutorCreation() {
-		FlowExecutor flowExecutor = (FlowExecutor)this.beanFactory.getBean("defaultExecutor");
-		assertNotNull("Incorrect FlowExecutor creation", flowExecutor);
+	public void testDefaultExecutor() {
+		FlowExecutorImpl flowExecutor = (FlowExecutorImpl)this.beanFactory.getBean("defaultExecutor");
+		assertSame(this.beanFactory.getBean("withPathWithWildcards"), flowExecutor.getDefinitionLocator());
+		AttributeMap attribs = ((FlowExecutionImplFactory)flowExecutor.getExecutionFactory()).getExecutionAttributes();
+		assertEquals(1, attribs.size()); // defaults have been applied
+		assertEquals(new Boolean(true), attribs.get(ApplicationViewSelector.ALWAYS_REDIRECT_ON_PAUSE_ATTRIBUTE));
 	}
+	
+	public void testSimpleExecutor() {
+		FlowExecutorImpl flowExecutor = (FlowExecutorImpl)this.beanFactory.getBean("simpleExecutor");
+		assertSame(this.beanFactory.getBean("withPathWithWildcards"), flowExecutor.getDefinitionLocator());
+		assertTrue(flowExecutor.getExecutionRepository() instanceof SimpleFlowExecutionRepository);
+		assertTrue(((SimpleFlowExecutionRepository)flowExecutor.getExecutionRepository()).isAlwaysGenerateNewNextKey());
+		AttributeMap attribs = ((FlowExecutionImplFactory)flowExecutor.getExecutionFactory()).getExecutionAttributes();
+		assertEquals(3, attribs.size());
+		assertEquals(new Boolean(true), attribs.get(ApplicationViewSelector.ALWAYS_REDIRECT_ON_PAUSE_ATTRIBUTE));
+		assertEquals("test", attribs.get("test"));
+		assertEquals(new Integer(1), attribs.get("test1"));
+		assertSame(StaticFlowExecutionListenerLoader.EMPTY_INSTANCE,
+				((FlowExecutionImplFactory)flowExecutor.getExecutionFactory()).getExecutionListenerLoader());
+	}
+	
+	public void testContinuationExecutor() {
+		FlowExecutorImpl flowExecutor = (FlowExecutorImpl)this.beanFactory.getBean("continuationExecutor");
+		assertSame(this.beanFactory.getBean("withPathWithWildcards"), flowExecutor.getDefinitionLocator());
+		assertTrue(flowExecutor.getExecutionRepository() instanceof ContinuationFlowExecutionRepository);
+		AttributeMap attribs = ((FlowExecutionImplFactory)flowExecutor.getExecutionFactory()).getExecutionAttributes();
+		assertEquals(1, attribs.size());
+		assertEquals(new Boolean(false), attribs.get(ApplicationViewSelector.ALWAYS_REDIRECT_ON_PAUSE_ATTRIBUTE));
+		ConditionalFlowExecutionListenerLoader ll = (ConditionalFlowExecutionListenerLoader)
+				((FlowExecutionImplFactory)flowExecutor.getExecutionFactory()).getExecutionListenerLoader();
+		assertEquals(1, ll.getListeners(new Flow("test")).length);
+		assertSame(this.beanFactory.getBean("listener1"), ll.getListeners(new Flow("test"))[0]);
+ 	}
+	
+	public void testClientExecutor() {
+		FlowExecutorImpl flowExecutor = (FlowExecutorImpl)this.beanFactory.getBean("clientExecutor");
+		assertSame(this.beanFactory.getBean("withPathWithWildcards"), flowExecutor.getDefinitionLocator());
+		assertTrue(flowExecutor.getExecutionRepository() instanceof ClientContinuationFlowExecutionRepository);
+		AttributeMap attribs = ((FlowExecutionImplFactory)flowExecutor.getExecutionFactory()).getExecutionAttributes();
+		assertEquals(1, attribs.size());
+		assertEquals(new Boolean(true), attribs.get(ApplicationViewSelector.ALWAYS_REDIRECT_ON_PAUSE_ATTRIBUTE));
+		ConditionalFlowExecutionListenerLoader ll = (ConditionalFlowExecutionListenerLoader)
+				((FlowExecutionImplFactory)flowExecutor.getExecutionFactory()).getExecutionListenerLoader();
+		assertEquals(2, ll.getListeners(new Flow("flow1")).length);
+		assertSame(this.beanFactory.getBean("listener1"), ll.getListeners(new Flow("flow1"))[0]);
+		assertSame(this.beanFactory.getBean("listener2"), ll.getListeners(new Flow("flow1"))[1]);
+		assertEquals(1, ll.getListeners(new Flow("flow2")).length);
+		assertSame(this.beanFactory.getBean("listener2"), ll.getListeners(new Flow("flow2"))[0]);
+		assertEquals(1, ll.getListeners(new Flow("flow3")).length);
+		assertSame(this.beanFactory.getBean("listener2"), ll.getListeners(new Flow("flow3"))[0]);
+ 	}
+	
+	public void testSingleKeyExecutor() {
+		FlowExecutorImpl flowExecutor = (FlowExecutorImpl)this.beanFactory.getBean("singleKeyExecutor");
+		assertSame(this.beanFactory.getBean("withPathWithWildcards"), flowExecutor.getDefinitionLocator());
+		assertTrue(flowExecutor.getExecutionRepository() instanceof SimpleFlowExecutionRepository);
+		assertFalse(((SimpleFlowExecutionRepository)flowExecutor.getExecutionRepository()).isAlwaysGenerateNewNextKey());
+		AttributeMap attribs = ((FlowExecutionImplFactory)flowExecutor.getExecutionFactory()).getExecutionAttributes();
+		assertEquals(1, attribs.size());
+		assertEquals(new Boolean(true), attribs.get(ApplicationViewSelector.ALWAYS_REDIRECT_ON_PAUSE_ATTRIBUTE));
+		assertSame(StaticFlowExecutionListenerLoader.EMPTY_INSTANCE,
+				((FlowExecutionImplFactory)flowExecutor.getExecutionFactory()).getExecutionListenerLoader());
+	}
+
 }
