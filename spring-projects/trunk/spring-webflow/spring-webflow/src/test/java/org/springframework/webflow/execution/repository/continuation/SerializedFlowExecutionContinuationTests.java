ee85c8d6eef108112637dd0a57e06cc7a2ea5a0b
klr8 <klr8@2e8e966d-6415-0410-94b9-dc4e65435d03>
2006-10-02 18:21:49 +0000

    Code review (fixed some JavaDoc issues).

--- spring-projects/trunk/spring-webflow/spring-webflow/src/test/java/org/springframework/webflow/execution/repository/continuation/SerializedFlowExecutionContinuationTests.java
+++ spring-projects/trunk/spring-webflow/spring-webflow/src/test/java/org/springframework/webflow/execution/repository/continuation/SerializedFlowExecutionContinuationTests.java
@@ -1,10 +1,22 @@
+/*
+ * Copyright 2002-2006 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.springframework.webflow.execution.repository.continuation;
 
 import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 
 import junit.framework.TestCase;
 
@@ -14,7 +26,13 @@ import org.springframework.webflow.engine.impl.FlowExecutionImplFactory;
 import org.springframework.webflow.execution.FlowExecution;
 import org.springframework.webflow.test.MockExternalContext;
 
+/**
+ * Unit tests for {@link SerializedFlowExecutionContinuation}.
+ * 
+ * @author Keith Donald
+ */
 public class SerializedFlowExecutionContinuationTests extends TestCase {
+	
 	public void testCreate() throws Exception {
 		FlowDefinition flow = new SimpleFlow();
 		FlowExecution execution = new FlowExecutionImplFactory().createFlowExecution(flow);
