cf9d1d3e06611815b4359b84bee41d385689d357
klr8 <klr8@2e8e966d-6415-0410-94b9-dc4e65435d03>
2006-10-12 14:00:43 +0000

    Cleanup of examples: * fixed copyright notices * removed unused directories

--- spring-projects/trunk/spring-webflow/spring-webflow-samples/itemlist/src/main/java/org/springframework/webflow/samples/itemlist/NewItemAction.java
+++ spring-projects/trunk/spring-webflow/spring-webflow-samples/itemlist/src/main/java/org/springframework/webflow/samples/itemlist/NewItemAction.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2004 the original author or authors.
+ * Copyright 2002-2006 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,6 +20,7 @@ import org.springframework.webflow.execution.Event;
 import org.springframework.webflow.execution.RequestContext;
 
 public class NewItemAction extends AbstractAction {
+
 	protected Event doExecute(RequestContext context) throws Exception {
 		return success();
 	}
