4b4e7241c97987fbbc220c845d243f62c845ecca
klr8 <klr8@2e8e966d-6415-0410-94b9-dc4e65435d03>
2006-10-12 14:00:59 +0000

    Cleanup of examples: * fixed copyright notices * removed unused directories

--- spring-projects/trunk/spring-webflow/spring-webflow-samples/numberguess/src/main/java/org/springframework/webflow/samples/numberguess/MastermindGame.java
+++ spring-projects/trunk/spring-webflow/spring-webflow-samples/numberguess/src/main/java/org/springframework/webflow/samples/numberguess/MastermindGame.java
@@ -1,17 +1,17 @@
 /*
- * Copyright 2002-2004 the original author or authors.
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- * 
- * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ * Copyright 2002-2006 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.springframework.webflow.samples.numberguess;
 
@@ -114,7 +114,7 @@ public class MastermindGame implements Serializable {
 
 		private String answer;
 
-		private List guessHistory = new ArrayList();
+		private List<GuessData> guessHistory = new ArrayList<GuessData>();
 
 		private GuessResult lastGuessResult;
 
@@ -146,7 +146,7 @@ public class MastermindGame implements Serializable {
 			return durationMilliseconds / 1000;
 		}
 
-		public Collection getGuessHistory() {
+		public Collection<GuessData> getGuessHistory() {
 			return guessHistory;
 		}
 
@@ -154,7 +154,7 @@ public class MastermindGame implements Serializable {
 			if (guessHistory.isEmpty()) {
 				return null;
 			}
-			return (GuessData)guessHistory.get(guessHistory.size() - 1);
+			return guessHistory.get(guessHistory.size() - 1);
 		}
 
 		public void recordGuessData(String guess, int rightPosition, int correctButWrongPosition) {
--- spring-projects/trunk/spring-webflow/spring-webflow-samples/numberguess/src/test/java/org/springframework/webflow/samples/numberguess/MastermindGameTests.java
+++ spring-projects/trunk/spring-webflow/spring-webflow-samples/numberguess/src/test/java/org/springframework/webflow/samples/numberguess/MastermindGameTests.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2002-2006 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.springframework.webflow.samples.numberguess;
 
 import junit.framework.TestCase;
@@ -7,7 +22,7 @@ import org.springframework.webflow.samples.numberguess.MastermindGame.GuessResul
 
 public class MastermindGameTests extends TestCase {
 
-	MastermindGame action = new MastermindGame();
+	private MastermindGame action = new MastermindGame();
 	
 	protected void setUp() {
 		action = new MastermindGame();
