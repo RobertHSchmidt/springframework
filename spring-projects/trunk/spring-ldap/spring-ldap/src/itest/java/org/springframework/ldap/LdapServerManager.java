9e8fe76ba4cc53eb6684f292a353a1fb765d9fea
ulsa <ulsa@2e8e966d-6415-0410-94b9-dc4e65435d03>
2006-10-14 18:48:28 +0000

    LDAP server is now shut down when Spring takes down the context.

--- spring-projects/trunk/spring-ldap/spring-ldap/src/itest/java/org/springframework/ldap/LdapServerManager.java
+++ spring-projects/trunk/spring-ldap/spring-ldap/src/itest/java/org/springframework/ldap/LdapServerManager.java
@@ -31,6 +31,7 @@ import javax.naming.directory.DirContext;
 import org.apache.directory.server.core.configuration.ShutdownConfiguration;
 import org.apache.directory.server.jndi.ServerContextFactory;
 import org.apache.directory.server.protocol.shared.store.LdifFileLoader;
+import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.ldap.ContextSource;
 import org.springframework.ldap.support.DefaultDirObjectFactory;
@@ -43,7 +44,7 @@ import org.springframework.ldap.support.DistinguishedName;
  * @author Mattias Arthursson
  * 
  */
-public class LdapServerManager implements InitializingBean {
+public class LdapServerManager implements InitializingBean, DisposableBean {
 
     private ContextSource contextSource;
 
@@ -51,7 +52,7 @@ public class LdapServerManager implements InitializingBean {
         this.contextSource = contextSource;
     }
 
-    public void shutdown() throws NamingException {
+    public void destroy() throws Exception {
         Properties env = new Properties();
         env.setProperty(Context.INITIAL_CONTEXT_FACTORY,
                 ServerContextFactory.class.getName());
