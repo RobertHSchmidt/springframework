905ed534ece9e8183e207e56f258e046c76b650e
kdonald <kdonald@2e8e966d-6415-0410-94b9-dc4e65435d03>
2006-10-25 01:04:43 +0000

    minor polishing, adding abstract methods

--- spring-projects/trunk/spring-webflow/spring-webflow/src/main/java/org/springframework/webflow/executor/support/FlowExecutorArgumentHandler.java
+++ spring-projects/trunk/spring-webflow/spring-webflow/src/main/java/org/springframework/webflow/executor/support/FlowExecutorArgumentHandler.java
@@ -23,18 +23,18 @@ import org.springframework.core.JdkVersion;
 import org.springframework.util.StringUtils;
 import org.springframework.webflow.context.ExternalContext;
 import org.springframework.webflow.execution.FlowExecutionContext;
-import org.springframework.webflow.executor.FlowExecutor;
+import org.springframework.webflow.execution.support.ExternalRedirect;
+import org.springframework.webflow.execution.support.FlowDefinitionRedirect;
 
 /**
- * Abstract base class for objects handling {@link FlowExecutor} arguments.
- * This class combines the two argument handling responsabilities
- * ({@link FlowExecutorArgumentExtractor argument extraction}
- * and {@link FlowExecutorArgumentExposer exposing arguments}) and makes sure
- * they are consistent, i.e. that exposed arguments can later be extracted
- * again.
+ * Abstract base class for objects handling
+ * {@link org.springframework.webflow.executor.FlowExecutor} arguments. This
+ * class combines the two argument handling responsabilities of ({@link FlowExecutorArgumentExtractor extraction}
+ * and {@link FlowExecutorArgumentExposer exposing}) and makes sure they are
+ * consistent, i.e. that exposed arguments can later be extracted again.
  * <p>
- * All argument names are configurable. Common convenience functionality is
- * also provided, e.g. a {@link #applyDefaultFlowId(String) default flow id},
+ * All argument names are configurable. Common convenience functionality is also
+ * provided, e.g. a {@link #applyDefaultFlowId(String) default flow id},
  * {@link #encodeValue(Object) URL encoding} and dealing with
  * {@link #makeRedirectUrlContextRelativeIfNecessary(String, ExternalContext) relative URLs}.
  * Subclasses are responsible for taking these settings into account when
@@ -54,37 +54,37 @@ public abstract class FlowExecutorArgumentHandler implements FlowExecutorArgumen
 	 * By default clients can send the id of the flow definition to be launched
 	 * using an argument with this name ("_flowId").
 	 */
-	private static final String FLOW_ID_ARGUMENT = "_flowId";
+	private static final String FLOW_ID_ARGUMENT_NAME = "_flowId";
 
 	/**
 	 * By default clients can send the key of a flow execution to be resumed
 	 * using an argument with this name ("_flowExecutionKey").
 	 */
-	private static final String FLOW_EXECUTION_KEY_ARGUMENT = "_flowExecutionKey";
+	private static final String FLOW_EXECUTION_KEY_ARGUMENT_NAME = "_flowExecutionKey";
 
 	/**
 	 * By default clients can send the event to be signaled in an argument with
 	 * this name ("_eventId").
 	 */
-	private static final String EVENT_ID_ARGUMENT = "_eventId";
+	private static final String EVENT_ID_ARGUMENT_NAME = "_eventId";
 
 	/**
 	 * Identifies a flow definition to launch a new execution for, defaults to
-	 * {@link #FLOW_ID_ARGUMENT}.
+	 * {@link #FLOW_ID_ARGUMENT_NAME}.
 	 */
-	private String flowIdArgumentName = FLOW_ID_ARGUMENT;
+	private String flowIdArgumentName = FLOW_ID_ARGUMENT_NAME;
 
 	/**
 	 * Input argument that identifies an existing flow execution to participate
-	 * in, defaults to {@link #FLOW_EXECUTION_KEY_ARGUMENT}.
+	 * in, defaults to {@link #FLOW_EXECUTION_KEY_ARGUMENT_NAME}.
 	 */
-	private String flowExecutionKeyArgumentName = FLOW_EXECUTION_KEY_ARGUMENT;
+	private String flowExecutionKeyArgumentName = FLOW_EXECUTION_KEY_ARGUMENT_NAME;
 
 	/**
 	 * Identifies an event that occured in an existing flow execution, defaults
-	 * to {@link #EVENT_ID_ARGUMENT}.
+	 * to {@link #EVENT_ID_ARGUMENT_NAME}.
 	 */
-	private String eventIdArgumentName = EVENT_ID_ARGUMENT;
+	private String eventIdArgumentName = EVENT_ID_ARGUMENT_NAME;
 
 	/**
 	 * The flow definition id to use if no flowId argument value can be
@@ -132,16 +132,16 @@ public abstract class FlowExecutorArgumentHandler implements FlowExecutorArgumen
 	}
 
 	/**
-	 * Sets the event id argument name, used to signal what user action
-	 * happened within a paused flow execution.
+	 * Sets the event id argument name, used to signal what user action happened
+	 * within a paused flow execution.
 	 */
 	public void setEventIdArgumentName(String eventIdArgumentName) {
 		this.eventIdArgumentName = eventIdArgumentName;
 	}
 
 	/**
-	 * Returns the <i>default</i> flowId argument value. If no flow id
-	 * argument is provided, the default acts as a fallback. Defaults to
+	 * Returns the <i>default</i> flowId argument value. If no flow id argument
+	 * is provided, the default acts as a fallback. Defaults to
 	 * <code>null</code>.
 	 */
 	public String getDefaultFlowId() {
@@ -151,9 +151,8 @@ public abstract class FlowExecutorArgumentHandler implements FlowExecutorArgumen
 	/**
 	 * Sets the default flowId argument value.
 	 * <p>
-	 * This value will be used if no flowId argument value can be extracted
-	 * from the request by the {@link #extractFlowId(ExternalContext)}
-	 * operation.
+	 * This value will be used if no flowId argument value can be extracted from
+	 * the request by the {@link #extractFlowId(ExternalContext)} operation.
 	 */
 	public void setDefaultFlowId(String defaultFlowId) {
 		this.defaultFlowId = defaultFlowId;
@@ -271,20 +270,41 @@ public abstract class FlowExecutorArgumentHandler implements FlowExecutorArgumen
 	public boolean isRedirectContextRelative() {
 		return redirectContextRelative;
 	}
-	
+
+	public abstract boolean isFlowIdPresent(ExternalContext context);
+
+	public abstract String extractFlowId(ExternalContext context) throws FlowExecutorArgumentExtractionException;
+
+	public abstract boolean isFlowExecutionKeyPresent(ExternalContext context);
+
+	public abstract String extractFlowExecutionKey(ExternalContext context)
+			throws FlowExecutorArgumentExtractionException;
+
+	public abstract boolean isEventIdPresent(ExternalContext context);
+
+	public abstract String extractEventId(ExternalContext context) throws FlowExecutorArgumentExtractionException;
+
+	public abstract String createFlowDefinitionUrl(FlowDefinitionRedirect flowDefinitionRedirect,
+			ExternalContext context);
+
+	public abstract String createFlowExecutionUrl(String flowExecutionKey, FlowExecutionContext flowExecution,
+			ExternalContext context);
+
+	public abstract String createExternalUrl(ExternalRedirect redirect, String flowExecutionKey, ExternalContext context);
+
 	public void exposeFlowExecutionContext(String flowExecutionKey, FlowExecutionContext context, Map model) {
 		if (flowExecutionKey != null) {
 			model.put(getFlowExecutionKeyAttributeName(), flowExecutionKey);
 		}
 		model.put(getFlowExecutionContextAttributeName(), context);
 	}
-	
+
 	// helpers for use in subclasses
-	
+
 	/**
 	 * Apply the configured default flow id to given extracted flow id.
-	 * @param extractedFlowId the extracted flow id, could be null if non
-	 * was available in the external context
+	 * @param extractedFlowId the extracted flow id, could be null if non was
+	 * available in the external context
 	 * @return the extracted flow id if not empty, the default flow id otherwise
 	 * (which could still be null if not set)
 	 * @see #getDefaultFlowId()
@@ -302,11 +322,11 @@ public abstract class FlowExecutorArgumentHandler implements FlowExecutorArgumen
 	protected String encodeValue(Object value) {
 		return value != null ? urlEncode(value.toString()) : "";
 	}
-	
+
 	/**
 	 * Make given redirect URL context relative if necessary. If the URL starts
-	 * with a slash ("/") it will be made relative to the current ServletContext,
-	 * i.e. relative to the web application root.
+	 * with a slash ("/") it will be made relative to the current
+	 * ServletContext, i.e. relative to the web application root.
 	 * @param url the original URL
 	 * @param context the external context
 	 * @return the processed URL
@@ -320,7 +340,7 @@ public abstract class FlowExecutorArgumentHandler implements FlowExecutorArgumen
 		res.append(url);
 		return res.toString();
 	}
-	
+
 	// internal helpers
 
 	/**
