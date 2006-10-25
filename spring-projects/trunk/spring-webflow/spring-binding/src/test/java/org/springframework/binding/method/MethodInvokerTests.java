57680b184c7420b625b02e2d65c6e117949d26ec
kdonald <kdonald@2e8e966d-6415-0410-94b9-dc4e65435d03>
2006-10-25 21:50:21 +0000

    tests

--- spring-projects/trunk/spring-webflow/spring-binding/src/test/java/org/springframework/binding/method/MethodInvokerTests.java
+++ spring-projects/trunk/spring-webflow/spring-binding/src/test/java/org/springframework/binding/method/MethodInvokerTests.java
@@ -17,19 +17,21 @@ package org.springframework.binding.method;
 
 import junit.framework.TestCase;
 
+import org.springframework.binding.expression.support.StaticExpression;
+
 /**
  * Unit tests for {@link org.springframework.binding.method.MethodInvoker}.
  * 
  * @author Erwin Vervaet
  */
 public class MethodInvokerTests extends TestCase {
-	
+
 	private MethodInvoker methodInvoker;
-	
+
 	protected void setUp() throws Exception {
 		this.methodInvoker = new MethodInvoker();
 	}
-	
+
 	public void testInvocationTargetException() {
 		try {
 			methodInvoker.invoke(new MethodSignature("test"), new TestObject(), null);
@@ -41,10 +43,47 @@ public class MethodInvokerTests extends TestCase {
 		}
 	}
 
+	public void testInvalidMethod() {
+		try {
+			methodInvoker.invoke(new MethodSignature("bogus"), new TestObject(), null);
+			fail();
+		}
+		catch (MethodInvocationException e) {
+			assertTrue(e.getTargetException() instanceof InvalidMethodSignatureException);
+		}
+	}
+
+	public void testBeanArg() {
+		Parameters parameters = new Parameters();
+		Bean bean = new Bean();
+		parameters.add(new Parameter(Bean.class, new StaticExpression(bean)));
+		MethodSignature method = new MethodSignature("testBeanArg", parameters);
+		assertSame(bean, methodInvoker.invoke(method, new TestObject(), null));
+	}
+
+	public void testPrimitiveArg() {
+		Parameters parameters = new Parameters();
+		parameters.add(new Parameter(Boolean.class, new StaticExpression(Boolean.TRUE)));
+		MethodSignature method = new MethodSignature("testPrimitiveArg", parameters);
+		assertEquals(Boolean.TRUE, methodInvoker.invoke(method, new TestObject(), null));
+	}
+
 	private static class TestObject {
-		
+
 		public void test() {
 			throw new IllegalArgumentException("just testing");
 		}
+
+		public Object testBeanArg(Bean bean) {
+			return bean;
+		}
+
+		public boolean testPrimitiveArg(boolean primitive) {
+			return primitive;
+		}
+	}
+
+	private static class Bean {
+		String value;
 	}
 }
