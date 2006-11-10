43f4cc4d25ef7b3509eabb3740b5b5e0d6866f86
klr8 <klr8@2e8e966d-6415-0410-94b9-dc4e65435d03>
2006-11-10 20:21:11 +0000

    ExternalContextHolder now uses a ThreadLocal instead of an InheritableThreadLocal (SWF-206).

--- spring-projects/trunk/spring-webflow/spring-webflow/src/main/java/org/springframework/webflow/context/ExternalContextHolder.java
+++ spring-projects/trunk/spring-webflow/spring-webflow/src/main/java/org/springframework/webflow/context/ExternalContextHolder.java
@@ -32,7 +32,7 @@ import org.springframework.util.Assert;
  */
 public final class ExternalContextHolder {
 
-	private static ThreadLocal externalContextHolder = new InheritableThreadLocal();
+	private static ThreadLocal externalContextHolder = new ThreadLocal();
 
 	/**
 	 * Associate the given ExternalContext with the current thread.
