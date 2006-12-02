cc476d58461b943c4e9159747a5ea24f674e6584
costin_leau <costin_leau@2e8e966d-6415-0410-94b9-dc4e65435d03>
2006-12-02 18:51:55 +0000

    + moved project to maven structure

--- spring-javaconfig/trunk/src/java/org/springframework/beans/factory/java/template/mvc/AnnotationHandlerMapping.java
+++
@@ -1,7 +0,0 @@
-package org.springframework.beans.factory.java.template.mvc;
-
-import org.springframework.web.servlet.handler.AbstractUrlHandlerMapping;
-
-public class AnnotationHandlerMapping extends AbstractUrlHandlerMapping {
-
-}
