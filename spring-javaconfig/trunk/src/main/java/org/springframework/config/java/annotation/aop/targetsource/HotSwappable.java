b54eb6d0556ad10efcfd85ccaf3318a4e28a7126
costin_leau <costin_leau@2e8e966d-6415-0410-94b9-dc4e65435d03>
2007-04-16 12:01:50 +0000

    fixed typo

--- spring-javaconfig/trunk/src/main/java/org/springframework/config/java/annotation/aop/targetsource/HotSwappable.java
+++ spring-javaconfig/trunk/src/main/java/org/springframework/config/java/annotation/aop/targetsource/HotSwappable.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.springframework.config.java.annotation.aop.targesource;
+package org.springframework.config.java.annotation.aop.targetsource;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
