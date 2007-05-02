7ef1b138371e2a59f239c236eceaf503e822631a
nebhale <nebhale@2e8e966d-6415-0410-94b9-dc4e65435d03>
2007-05-02 13:06:12 +0000

    Moved the package for the ANT task

--- spring-aws/trunk/spring-aws-ant/src/main/java/org/springframework/aws/ant/Upload.java
+++ spring-aws/trunk/spring-aws-ant/src/main/java/org/springframework/aws/ant/Upload.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.springframework.aws;
+package org.springframework.aws.ant;
 
 import java.io.File;
 import java.io.IOException;
