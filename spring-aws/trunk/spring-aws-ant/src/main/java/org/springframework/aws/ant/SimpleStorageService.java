1d5d1d1e6ac5e1e5930463aaa93a1f69660087a1
nebhale <nebhale@2e8e966d-6415-0410-94b9-dc4e65435d03>
2007-05-02 13:06:12 +0000

    Moved the package for the ANT task

--- spring-aws/trunk/spring-aws-ant/src/main/java/org/springframework/aws/ant/SimpleStorageService.java
+++ spring-aws/trunk/spring-aws-ant/src/main/java/org/springframework/aws/ant/SimpleStorageService.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.springframework.aws;
+package org.springframework.aws.ant;
 
 import java.io.IOException;
 import java.util.ArrayList;
