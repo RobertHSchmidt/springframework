7cd0821dd301772e3250cc86adec26fe45c609fa
kdonald <kdonald@2e8e966d-6415-0410-94b9-dc4e65435d03>
2007-08-31 19:52:21 +0000

    Branch for SWF-367

---
+++ spring-webflow/branches/SWF-367/spring-binding/src/main/java/org/springframework/binding/expression/el/ELContextFactory.java
@@ -0,0 +1,28 @@
+package org.springframework.binding.expression.el;
+
+import javax.el.ELContext;
+import javax.el.ELResolver;
+
+/**
+ * A factory for creating a EL context object that will be used to evaluate a target object of an EL expression.
+ * 
+ * @author Jeremy Grelle
+ */
+public interface ELContextFactory {
+
+	/**
+	 * Configures and returns an {@link ELContext} to be used in parsing EL expressions.
+	 * @return ELContext The configured ELContext instance for parsing expressions.
+	 */
+	public ELContext getParseContext();
+
+	/**
+	 * Configures and returns an {@link ELContext} to be used in evaluating EL expressions on the given base target
+	 * object. In certain environments the target will be null and the base object of the expression is expected to be
+	 * resolved via the ELContext's {@link ELResolver} chain.
+	 * @param target The base object for the expression evaluation.
+	 * @return ELContext The configured ELContext instance for evaluating expressions.
+	 */
+	public ELContext getEvaluationContext(Object target);
+
+}
