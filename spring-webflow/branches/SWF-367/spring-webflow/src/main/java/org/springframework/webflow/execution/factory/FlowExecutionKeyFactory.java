19108f2398a8bc3e07d4f28f58b53213da68bfb5
kdonald <kdonald@2e8e966d-6415-0410-94b9-dc4e65435d03>
2007-09-07 01:25:56 +0000

    Changed key factory interface to add execution argument

--- spring-webflow/branches/SWF-367/spring-webflow/src/main/java/org/springframework/webflow/execution/factory/FlowExecutionKeyFactory.java
+++ spring-webflow/branches/SWF-367/spring-webflow/src/main/java/org/springframework/webflow/execution/factory/FlowExecutionKeyFactory.java
@@ -1,7 +1,8 @@
 package org.springframework.webflow.execution.factory;
 
+import org.springframework.webflow.execution.FlowExecution;
 import org.springframework.webflow.execution.FlowExecutionKey;
 
 public interface FlowExecutionKeyFactory {
-	public FlowExecutionKey getKey();
+	public FlowExecutionKey getKey(FlowExecution execution);
 }
