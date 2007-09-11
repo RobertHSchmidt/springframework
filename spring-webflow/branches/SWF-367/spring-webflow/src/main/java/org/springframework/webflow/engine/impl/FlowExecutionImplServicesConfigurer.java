54be2fe29848d25b5bcd0feecf41a21be3d9255e
kdonald <kdonald@2e8e966d-6415-0410-94b9-dc4e65435d03>
2007-09-11 15:07:28 +0000

    Deleted unused tests

---
+++ spring-webflow/branches/SWF-367/spring-webflow/src/main/java/org/springframework/webflow/engine/impl/FlowExecutionImplServicesConfigurer.java
@@ -0,0 +1,147 @@
+/*
+ * Copyright 2004-2007 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.webflow.engine.impl;
+
+import java.io.Serializable;
+
+import org.springframework.util.Assert;
+import org.springframework.webflow.core.collection.AttributeMap;
+import org.springframework.webflow.core.collection.CollectionUtils;
+import org.springframework.webflow.core.collection.MutableAttributeMap;
+import org.springframework.webflow.execution.FlowExecution;
+import org.springframework.webflow.execution.FlowExecutionKey;
+import org.springframework.webflow.execution.FlowExecutionRequestRedirector;
+import org.springframework.webflow.execution.factory.FlowExecutionKeyFactory;
+import org.springframework.webflow.execution.factory.FlowExecutionListenerLoader;
+import org.springframework.webflow.execution.factory.StaticFlowExecutionListenerLoader;
+import org.springframework.webflow.util.RandomGuidUidGenerator;
+
+abstract class FlowExecutionImplServicesConfigurer {
+
+	/**
+	 * The strategy for loading listeners that should observe executions of a flow definition. The default simply loads
+	 * an empty static listener list.
+	 */
+	private FlowExecutionListenerLoader executionListenerLoader = StaticFlowExecutionListenerLoader.EMPTY_INSTANCE;
+
+	/**
+	 * System execution attributes that may influence flow execution behavior. The default is an empty map.
+	 */
+	private AttributeMap executionAttributes = CollectionUtils.EMPTY_ATTRIBUTE_MAP;
+
+	/**
+	 * The factory used to assign keys to flow executions that need to be persisted.
+	 */
+	private FlowExecutionKeyFactory executionKeyFactory = new RandomFlowExecutionKeyFactory();
+
+	/**
+	 * A service that can instruct clients to take specific actions.
+	 */
+	private FlowExecutionRequestRedirector executionRequestRedirector = new NoOpFlowExecutionRequestRedirector();
+
+	/**
+	 * Sets the attributes to apply to flow executions created by this factory. Execution attributes may affect flow
+	 * execution behavior.
+	 * @param executionAttributes flow execution system attributes
+	 */
+	public void setExecutionAttributes(AttributeMap executionAttributes) {
+		Assert.notNull(executionAttributes, "The execution attributes map is required");
+		this.executionAttributes = executionAttributes;
+	}
+
+	/**
+	 * Sets the strategy for loading listeners that should observe executions of a flow definition. Allows full control
+	 * over what listeners should apply for executions of a flow definition.
+	 */
+	public void setExecutionListenerLoader(FlowExecutionListenerLoader executionListenerLoader) {
+		Assert.notNull(executionListenerLoader, "The execution listener loader is required");
+		this.executionListenerLoader = executionListenerLoader;
+	}
+
+	/**
+	 * Sets the strategy for generating flow execution keys for persistent flow executions.
+	 */
+	public void setExecutionKeyFactory(FlowExecutionKeyFactory executionKeyFactory) {
+		this.executionKeyFactory = executionKeyFactory;
+	}
+
+	/**
+	 * Sets the strategy for redirecting a flow execution request.
+	 */
+	public void setExecutionRequestRedirector(FlowExecutionRequestRedirector executionRequestRedirector) {
+		this.executionRequestRedirector = executionRequestRedirector;
+	}
+
+	protected FlowExecution configureServices(FlowExecutionImpl execution) {
+		execution.setAttributes(executionAttributes);
+		execution.setListeners(executionListenerLoader.getListeners(execution.getDefinition()));
+		execution.setKeyFactory(executionKeyFactory);
+		execution.setRequestRedirector(executionRequestRedirector);
+		return execution;
+	}
+
+	/**
+	 * Simply throws unsupported operation exceptions.
+	 */
+	private static class NoOpFlowExecutionRequestRedirector implements FlowExecutionRequestRedirector {
+		public void sendFlowExecutionRedirect(FlowExecutionKey key) {
+			throw new UnsupportedOperationException("Not Supported");
+		}
+
+		public void sendFlowDefinitionRedirect(String flowId, MutableAttributeMap input) {
+			throw new UnsupportedOperationException("Not Supported");
+		}
+
+		public void sendExternalRedirect(String resourceUri) {
+			throw new UnsupportedOperationException("Not Supported");
+		}
+	}
+
+	/**
+	 * Generates random flow execution keys.
+	 */
+	private static class RandomFlowExecutionKeyFactory implements FlowExecutionKeyFactory {
+		private RandomGuidUidGenerator idGenerator = new RandomGuidUidGenerator();
+
+		public FlowExecutionKey getKey(FlowExecution execution) {
+			return new SimpleFlowExecutionKey(idGenerator.generateUid());
+		}
+
+		private static class SimpleFlowExecutionKey extends FlowExecutionKey {
+			private Serializable value;
+
+			public SimpleFlowExecutionKey(Serializable value) {
+				this.value = value;
+			}
+
+			public boolean equals(Object o) {
+				if (!(o instanceof SimpleFlowExecutionKey)) {
+					SimpleFlowExecutionKey key = (SimpleFlowExecutionKey) o;
+					return this.value.equals(key.value);
+				}
+				return false;
+			}
+
+			public int hashCode() {
+				return this.value.hashCode();
+			}
+
+			public String toString() {
+				return value.toString();
+			}
+		}
+	}
+}
