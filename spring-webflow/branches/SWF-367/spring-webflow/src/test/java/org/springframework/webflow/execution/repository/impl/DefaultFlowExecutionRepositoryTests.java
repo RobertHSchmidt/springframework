69bc8e42e246aca4f83226272433b3c8853d0a33
kdonald <kdonald@2e8e966d-6415-0410-94b9-dc4e65435d03>
2007-09-13 20:01:36 +0000

    repository tests

---
+++ spring-webflow/branches/SWF-367/spring-webflow/src/test/java/org/springframework/webflow/execution/repository/impl/DefaultFlowExecutionRepositoryTests.java
@@ -0,0 +1,10 @@
+package org.springframework.webflow.execution.repository.impl;
+
+import junit.framework.TestCase;
+
+public class DefaultFlowExecutionRepositoryTests extends TestCase {
+
+	protected void setUp() {
+
+	}
+}
