f78b7c02a9aed4af49677a72f9835f64fc67f064
jeremyg484 <jeremyg484@2e8e966d-6415-0410-94b9-dc4e65435d03>
2007-09-14 22:00:22 +0000

    Adding RequestContextHolder to allow third party integration modules to gain easier access to the RequestContext

---
+++ spring-webflow/branches/SWF-367/spring-webflow/src/main/java/org/springframework/webflow/execution/RequestContextHolder.java
@@ -0,0 +1,41 @@
+package org.springframework.webflow.execution;
+
+import org.springframework.util.Assert;
+
+/**
+ * Simple holder class that associates a {@link RequestContext} instance with the current thread. The RequestContext
+ * will not be inherited by any child threads spawned by the current thread.
+ * <p>
+ * Used as a central holder for the current RequestContext in Spring Web Flow, wherever necessary. Often used by
+ * integration artifacts needing access to the current flow execution.
+ * 
+ * @see RequestContext
+ * 
+ * @author Jeremy Grelle
+ */
+public class RequestContextHolder {
+
+	private static final ThreadLocal requestContextHolder = new ThreadLocal();
+
+	/**
+	 * Associate the given RequestContext with the current thread.
+	 * @param requestContext the current RequestContext, or <code>null</code> to reset the thread-bound context
+	 */
+	public static void setRequestContext(RequestContext requestContext) {
+		requestContextHolder.set(requestContext);
+	}
+
+	/**
+	 * Return the RequestContext associated with the current thread, if any.
+	 * @return the current RequestContext
+	 * @throws IllegalStateException if no RequestContext is bound to this thread
+	 */
+	public static RequestContext getRequestContext() {
+		Assert.state(requestContextHolder.get() != null, "No external context is bound to this thread");
+		return (RequestContext) requestContextHolder.get();
+	}
+
+	// not instantiable
+	private RequestContextHolder() {
+	}
+}
