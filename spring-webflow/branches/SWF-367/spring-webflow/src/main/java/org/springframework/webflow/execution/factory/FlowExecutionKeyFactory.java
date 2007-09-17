723f7453729d7bd6241aa1115587f838f596b884
kdonald <kdonald@2e8e966d-6415-0410-94b9-dc4e65435d03>
2007-09-17 15:41:13 +0000

    Simpliications of factory package

--- spring-webflow/branches/SWF-367/spring-webflow/src/main/java/org/springframework/webflow/execution/factory/FlowExecutionKeyFactory.java
+++ spring-webflow/branches/SWF-367/spring-webflow/src/main/java/org/springframework/webflow/execution/factory/FlowExecutionKeyFactory.java
@@ -3,6 +3,20 @@ package org.springframework.webflow.execution.factory;
 import org.springframework.webflow.execution.FlowExecution;
 import org.springframework.webflow.execution.FlowExecutionKey;
 
+/**
+ * A factory for creating flow execution keys. Used to generate a persistent identity for a flow execution that needs to
+ * be persisted.
+ * 
+ * @author Keith Donald
+ */
 public interface FlowExecutionKeyFactory {
+
+	/**
+	 * Get the key to assign to the flow execution. This factory simply generates the key to assign, it does not
+	 * actually perform the key assignment.
+	 * 
+	 * @param execution the flow execution
+	 * @return the key to assign to the flow execution
+	 */
 	public FlowExecutionKey getKey(FlowExecution execution);
 }
