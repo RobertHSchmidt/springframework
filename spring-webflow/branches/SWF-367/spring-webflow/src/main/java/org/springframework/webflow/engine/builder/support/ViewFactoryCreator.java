2584b7bd08fb0e7d001a8a3d416c3a31dae40876
kdonald <kdonald@2e8e966d-6415-0410-94b9-dc4e65435d03>
2007-09-27 17:21:53 +0000

    Xml flow builder

--- spring-webflow/branches/SWF-367/spring-webflow/src/main/java/org/springframework/webflow/engine/builder/support/ViewFactoryCreator.java
+++ spring-webflow/branches/SWF-367/spring-webflow/src/main/java/org/springframework/webflow/engine/builder/support/ViewFactoryCreator.java
@@ -1,14 +1,13 @@
 package org.springframework.webflow.engine.builder.support;
 
+import org.springframework.binding.expression.Expression;
+import org.springframework.webflow.execution.Action;
 import org.springframework.webflow.execution.ViewFactory;
 
 public interface ViewFactoryCreator {
 
-	/**
-	 * Create and return a view factory
-	 * @param viewName the name of the view to be produced by the created view factory
-	 * @return the view factory
-	 */
-	ViewFactory createViewFactory(String viewName);
+	public ViewFactory createViewFactory(Expression viewName);
+
+	public Action createFinalResponseAction(Expression viewName);
 
 }
