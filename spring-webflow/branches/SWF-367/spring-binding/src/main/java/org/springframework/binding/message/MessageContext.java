4c209dbedc16e6e223aa468af209d722ca4bb061
kdonald <kdonald@2e8e966d-6415-0410-94b9-dc4e65435d03>
2007-10-01 15:51:32 +0000

    message context

--- spring-webflow/branches/SWF-367/spring-binding/src/main/java/org/springframework/binding/message/MessageContext.java
+++ spring-webflow/branches/SWF-367/spring-binding/src/main/java/org/springframework/binding/message/MessageContext.java
@@ -1,7 +1,19 @@
 package org.springframework.binding.message;
 
+/**
+ * A context for accessing and recording messages for display.
+ */
 public interface MessageContext {
-	public Message[] getMessages(String objectId);
 
-	public void addMessage(String objectId, MessageResolver messageResolver);
+	/**
+	 * Get all messages in this context. The messages returned should be suitable for display as-is.
+	 * @return the messages
+	 */
+	public Message[] getMessages();
+
+	/**
+	 * Add a new message to this context.
+	 * @param messageResolver the resolver that will resolve the message to be added
+	 */
+	public void addMessage(MessageResolver messageResolver);
 }
