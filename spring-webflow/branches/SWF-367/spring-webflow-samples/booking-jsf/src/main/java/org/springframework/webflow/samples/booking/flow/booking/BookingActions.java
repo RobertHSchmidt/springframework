c8ce88d390631b31cdfff8767012ae68ccaa8366
jeremyg484 <jeremyg484@2e8e966d-6415-0410-94b9-dc4e65435d03>
2007-10-04 15:52:41 +0000

    Updated for EL and Message support changes

--- spring-webflow/branches/SWF-367/spring-webflow-samples/booking-jsf/src/main/java/org/springframework/webflow/samples/booking/flow/booking/BookingActions.java
+++ spring-webflow/branches/SWF-367/spring-webflow-samples/booking-jsf/src/main/java/org/springframework/webflow/samples/booking/flow/booking/BookingActions.java
@@ -2,10 +2,9 @@ package org.springframework.webflow.samples.booking.flow.booking;
 
 import java.util.Calendar;
 
-import javax.faces.application.FacesMessage;
-import javax.faces.context.FacesContext;
 import javax.persistence.EntityManager;
 
+import org.springframework.binding.message.Messages;
 import org.springframework.webflow.action.MultiAction;
 import org.springframework.webflow.execution.Event;
 import org.springframework.webflow.execution.RequestContext;
@@ -54,15 +53,12 @@ public class BookingActions extends MultiAction {
 	Calendar calendar = Calendar.getInstance();
 	calendar.add(Calendar.DAY_OF_MONTH, -1);
 	if (booking.getCheckinDate().before(calendar.getTime())) {
-	    FacesContext.getCurrentInstance().addMessage("checkinDate",
-		    new FacesMessage(FacesMessage.SEVERITY_ERROR, "Check in date must be a future date", ""));
+	    context.getMessageContext()
+		    .addMessage(Messages.error("checkinDate", "Check in date must be a future date"));
 	    return error();
 	} else if (!booking.getCheckinDate().before(booking.getCheckoutDate())) {
-	    FacesContext.getCurrentInstance()
-		    .addMessage(
-			    "checkoutDate",
-			    new FacesMessage(FacesMessage.SEVERITY_ERROR,
-				    "Check out date must be later than check in date", ""));
+	    context.getMessageContext().addMessage(
+		    Messages.error("checkoutDate", "Check out date must be later than check in date"));
 	    return error();
 	}
 	return success();
