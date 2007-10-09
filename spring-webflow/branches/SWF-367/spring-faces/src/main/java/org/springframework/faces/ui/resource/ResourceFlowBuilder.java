16126c301de6890410250e0672e33ff9122c2385
kdonald <kdonald@2e8e966d-6415-0410-94b9-dc4e65435d03>
2007-10-09 21:27:31 +0000

    made compile with builder changes

--- spring-webflow/branches/SWF-367/spring-faces/src/main/java/org/springframework/faces/ui/resource/ResourceFlowBuilder.java
+++ spring-webflow/branches/SWF-367/spring-faces/src/main/java/org/springframework/faces/ui/resource/ResourceFlowBuilder.java
@@ -1,6 +1,5 @@
 package org.springframework.faces.ui.resource;
 
-import org.springframework.webflow.core.collection.AttributeMap;
 import org.springframework.webflow.engine.EndState;
 import org.springframework.webflow.engine.Flow;
 import org.springframework.webflow.engine.builder.FlowBuilderException;
@@ -9,23 +8,16 @@ import org.springframework.webflow.engine.builder.support.AbstractFlowBuilder;
 /**
  * Builder for generating the "resources" flow which is responsible for serving static resources from the classpath.
  * @author Jeremy Grelle
- * 
  */
 public class ResourceFlowBuilder extends AbstractFlowBuilder {
 
-	public static final String FLOW_ID = "resources";
+	protected Flow createFlow() {
+		return new Flow("resources");
+	}
 
 	public void buildStates() throws FlowBuilderException {
-
 		Flow resourcesFlow = getFlow();
-
 		EndState endState = new EndState(resourcesFlow, "renderResource");
 		endState.setFinalResponseAction(new ResolveAndRenderResourceAction());
 	}
-
-	public void init(String flowId, AttributeMap attributes) throws FlowBuilderException {
-		Flow resourcesFlow = new Flow(FLOW_ID);
-		setFlow(resourcesFlow);
-	}
-
 }
