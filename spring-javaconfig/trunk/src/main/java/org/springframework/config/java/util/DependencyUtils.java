a93bd082cfb88e58474708916cc71320462a9b92
cbeams <cbeams@2e8e966d-6415-0410-94b9-dc4e65435d03>
2007-11-25 04:22:42 +0000

    Changes per code review with Rod.  Implemented caching for DependencyUtils.isAopAvailable(), added header to DependencyUtils.  Also added header to project code template settings.

--- spring-javaconfig/trunk/src/main/java/org/springframework/config/java/util/DependencyUtils.java
+++ spring-javaconfig/trunk/src/main/java/org/springframework/config/java/util/DependencyUtils.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2002-2007 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.springframework.config.java.util;
 
 /**
@@ -11,17 +26,23 @@ public final class DependencyUtils {
 	private DependencyUtils() {
 	}
 
+	private static boolean isAopAvailable;
+
+	static {
+		try {
+			Class.forName("org.springframework.aop.TargetSource");
+			isAopAvailable = true;
+		}
+		catch (ClassNotFoundException ex) {
+			isAopAvailable = false;
+		}
+	}
+
 	/**
 	 * Inspects the classpath to determine whether key AOP resources are
 	 * present.
 	 */
 	public static boolean isAopAvailable() {
-		try {
-			Class.forName("org.springframework.aop.TargetSource");
-			return true;
-		}
-		catch (ClassNotFoundException e) {
-			return false;
-		}
+		return isAopAvailable;
 	}
 }
