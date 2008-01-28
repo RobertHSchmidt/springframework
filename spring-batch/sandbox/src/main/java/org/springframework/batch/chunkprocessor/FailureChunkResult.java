49293c652b12f1230e12e306e1de38ac874cbb53
nebhale <nebhale@2e8e966d-6415-0410-94b9-dc4e65435d03>
2008-01-28 15:33:46 +0000

    Adding skipped items to ChunkResult

--- spring-batch/sandbox/src/main/java/org/springframework/batch/chunkprocessor/FailureChunkResult.java
+++ spring-batch/sandbox/src/main/java/org/springframework/batch/chunkprocessor/FailureChunkResult.java
@@ -3,7 +3,7 @@ package org.springframework.batch.chunkprocessor;
 public class FailureChunkResult extends ChunkResult {
 
 	public FailureChunkResult(Long chunkId) {
-		super(FAILURE, chunkId);
+		super(FAILURE, chunkId, null);
 	}
 
 }
--- spring-batch/sandbox/src/main/java/org/springframework/batch/chunkprocessor/SuccessChunkResult.java
+++ spring-batch/sandbox/src/main/java/org/springframework/batch/chunkprocessor/SuccessChunkResult.java
@@ -1,9 +1,11 @@
 package org.springframework.batch.chunkprocessor;
 
+import java.util.List;
+
 public class SuccessChunkResult extends ChunkResult {
 
-	public SuccessChunkResult(Long chunkId) {
-		super(SUCCESS, chunkId);
+	public SuccessChunkResult(Long chunkId, List skippedItems) {
+		super(SUCCESS, chunkId, skippedItems);
 	}
 
 }
