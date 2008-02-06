c310643f101a64ac7016c5798bc122557e23bcdc
cbeams <cbeams@2e8e966d-6415-0410-94b9-dc4e65435d03>
2008-02-06 18:15:48 +0000

    cleaning up implementation of StandardBeanConfigurationListener

--- spring-javaconfig/branches/SJC-63/src/main/java/org/springframework/config/java/process/StandardBeanConfigurationListener.java
+++ spring-javaconfig/branches/SJC-63/src/main/java/org/springframework/config/java/process/StandardBeanConfigurationListener.java
@@ -17,43 +17,44 @@ class StandardBeanConfigurationListener extends ConfigurationListenerSupport {
 		return !(StandardBeanMethodProcessor.findBeanCreationMethods(configurationClass).isEmpty());
 	}
 
-	private final HashMap<Class<?>, List<String>> noArgMethodsSeen2 = new HashMap<Class<?>, List<String>>();
+	private final HashMap<Class<?>, List<String>> noArgMethodsSeen = new HashMap<Class<?>, List<String>>();
 
 	@Override
 	public void handleEvent(Reactor reactor, MethodEvent event) {
 		Method m = event.method;
+		Bean beanAnnotation = AnnotationUtils.findAnnotation(m, Bean.class);
+		if (beanAnnotation == null)
+			return;
+
 		Class<?> configurationClass = event.clazz;
 		String configurationBeanName = event.configurationBeanName;
 		BeanNamingStrategy beanNamingStrategy = event.beanNamingStrategy;
 		ConfigurableListableBeanFactory owningBeanFactory = event.owningBeanFactory;
-		Bean beanAnnotation = AnnotationUtils.findAnnotation(m, Bean.class);
 		String beanName = beanNamingStrategy.getBeanName(m);
 		ConfigurationProcessor cp = (ConfigurationProcessor) reactor;
 
-		if (!noArgMethodsSeen2.containsKey(configurationClass))
-			noArgMethodsSeen2.put(configurationClass, new ArrayList<String>());
+		if (!noArgMethodsSeen.containsKey(configurationClass))
+			noArgMethodsSeen.put(configurationClass, new ArrayList<String>());
 
-		if (beanAnnotation != null) {
-			if (!noArgMethodsSeen2.get(configurationClass).contains(beanName)) {
-				// If the bean already exists in the factory, don't emit
-				// a bean definition. This may or may not be legal,
-				// depending on whether the @Bean annotation allows
-				// overriding
-				if (owningBeanFactory.containsLocalBean(beanName)) {
-					if (!beanAnnotation.allowOverriding()) {
-						String message = String.format(
-								"A bean named '%s' already exists. Consider using @Bean(allowOverriding=true)",
-								beanName);
-						throw new IllegalStateException(message);
-					}
-					// Don't emit a bean definition
-					return;
-				}
-				noArgMethodsSeen2.get(configurationClass).add(beanName);
-				cp.generateBeanDefinitionFromBeanCreationMethod(owningBeanFactory, configurationBeanName,
-						configurationClass, beanName, m, beanAnnotation);
+		if (noArgMethodsSeen.get(configurationClass).contains(beanName))
+			return;
+
+		// If the bean already exists in the factory, don't emit
+		// a bean definition. This may or may not be legal,
+		// depending on whether the @Bean annotation allows
+		// overriding
+		if (owningBeanFactory.containsLocalBean(beanName)) {
+			if (!beanAnnotation.allowOverriding()) {
+				String message = String.format(
+						"A bean named '%s' already exists. Consider using @Bean(allowOverriding=true)", beanName);
+				throw new IllegalStateException(message);
 			}
+			// Don't emit a bean definition
+			return;
 		}
-	}
+		noArgMethodsSeen.get(configurationClass).add(beanName);
 
+		cp.generateBeanDefinitionFromBeanCreationMethod(owningBeanFactory, configurationBeanName, configurationClass,
+				beanName, m, beanAnnotation);
+	}
 }
