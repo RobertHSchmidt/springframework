932b443734319a777e22db555fcc8b6578dd7028
cbeams <cbeams@2e8e966d-6415-0410-94b9-dc4e65435d03>
2008-02-09 01:24:55 +0000

    Refactoring method placement

--- spring-javaconfig/branches/SJC-63/src/main/java/org/springframework/config/java/process/ProcessUtils.java
+++ spring-javaconfig/branches/SJC-63/src/main/java/org/springframework/config/java/process/ProcessUtils.java
@@ -20,9 +20,7 @@ import java.lang.reflect.Modifier;
 
 import org.springframework.beans.factory.BeanDefinitionStoreException;
 import org.springframework.beans.factory.annotation.Autowire;
-import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
-import org.springframework.beans.factory.support.AbstractBeanDefinition;
 import org.springframework.beans.factory.support.RootBeanDefinition;
 import org.springframework.config.java.annotation.Bean;
 import org.springframework.config.java.annotation.Configuration;
@@ -137,68 +135,4 @@ final class ProcessUtils {
 
 	}
 
-	/**
-	 * Return the class useful for processing for the given bean. Will return
-	 * null if no bean is not valid or if no class can be found. Will throw an
-	 * exception if the found class is invalid.
-	 * 
-	 * No check for
-	 * @Bean or
-	 * @Configuration annotations is made.
-	 * 
-	 * @param beanName
-	 * @param clbf
-	 * @return
-	 */
-	static Class<?> getBeanClass(String beanName, ConfigurableListableBeanFactory clbf) {
-
-		BeanDefinition bd = clbf.getBeanDefinition(beanName);
-
-		if (!(isEligibleForConfigurationProcessing(bd) && bd instanceof AbstractBeanDefinition))
-			return null;
-
-		// required for updating the bean class
-		AbstractBeanDefinition definition = (AbstractBeanDefinition) bd;
-
-		// TODO: check for FactoryBean/factory-method type of beans
-		// hard since we are a BFPP and it's impossible to get the actual
-		// configuration instance/class w/o initilizing the factory-method/FB
-		// even for non @Configuration cases.
-
-		if (definition.hasBeanClass())
-			return definition.getBeanClass();
-
-		// load the class (changes in the lazy loading code part of
-		// spring core)
-
-		// TODO: add support for factory-method beans (and other not-normal
-		// beans) this requires transforming the BFPP into a BPP and might
-		// require multiple instantion of the configuration class.
-		if (bd.getBeanClassName() != null) {
-			try {
-				return org.springframework.util.ClassUtils.forName(bd.getBeanClassName());
-			}
-			catch (ClassNotFoundException e) {
-				throw new IllegalArgumentException("Bean class '" + bd.getBeanClassName() + "' not found");
-			}
-		}
-
-		return null;
-	}
-
-	/**
-	 * Determines if the given bean definition is eligible for configuration
-	 * processsing by a ConfigurationProcessor. Abstract beans as well as bean
-	 * definitions w/o classes are excluded.
-	 * 
-	 * @param def
-	 * @return
-	 */
-	private static boolean isEligibleForConfigurationProcessing(BeanDefinition def) {
-		if (def.isAbstract() || (!StringUtils.hasText(def.getBeanClassName()))) {
-			return false;
-		}
-
-		return true;
-	}
 }
