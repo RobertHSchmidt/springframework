5303a71f46b311a6de34668e88de3b907df0a873
cbeams <cbeams@2e8e966d-6415-0410-94b9-dc4e65435d03>
2008-04-16 19:57:53 +0000

    + fleshed out recipe for processing a configuration class in RefactoredReflectiveJavaConfigBeanDefinitionReader

---
+++ spring-javaconfig/branches/SJC-87/core/src/main/java/org/springframework/config/java/model/ReflectiveJavaConfigurationModelPopulator.java
@@ -0,0 +1,15 @@
+package org.springframework.config.java.model;
+
+public class ReflectiveJavaConfigurationModelPopulator {
+
+	private final JavaConfigurationModel model;
+
+	public ReflectiveJavaConfigurationModelPopulator(JavaConfigurationModel model) {
+		this.model = model;
+	}
+
+	public void addToModel(Class<?> configurationClass) {
+		// TODO Auto-generated method stub
+	}
+
+}
