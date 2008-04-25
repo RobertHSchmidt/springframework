0bd73c406c1c070b449243479a10c84adb433901
cbeams <cbeams@2e8e966d-6415-0410-94b9-dc4e65435d03>
2008-04-25 04:22:48 +0000

    SJC-87: factored out some duplicate code into util method

--- spring-javaconfig/branches/SJC-87/core/src/test/java/org/springframework/config/java/test/Assert.java
+++ spring-javaconfig/branches/SJC-87/core/src/test/java/org/springframework/config/java/test/Assert.java
@@ -17,10 +17,19 @@ public class Assert {
 	 * @param expected expected number of beans
 	 */
 	public static void assertBeanDefinitionCount(ConfigurableApplicationContext ctx, int expected) {
-		int actual = 0;
+		int actual = getNonInternalBeanDefinitionCount(ctx);
+		assertEquals(expected, actual);
+	}
+
+	/**
+	 * Returns the number of beans in this <var>ctx</var> that are not JavaConfig-internal bean definitions.
+	 * Said another way, returns the number of beans that the end user has registered.
+	 */
+	public static int getNonInternalBeanDefinitionCount(ConfigurableApplicationContext ctx) {
+		int cx = 0;
 		for (String name : ctx.getBeanDefinitionNames())
 			if(ctx.getBeanFactory().getBeanDefinition(name).getAttribute(Constants.JAVA_CONFIG_IGNORE) == null)
-				actual++;
-		assertEquals(expected, actual);
+				cx++;
+		return cx;
 	}
 }
