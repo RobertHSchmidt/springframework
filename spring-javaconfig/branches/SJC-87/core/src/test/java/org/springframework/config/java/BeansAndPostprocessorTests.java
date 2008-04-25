5ac61964a1ed572c9ec555c76cbce035a7d2f50d
cbeams <cbeams@2e8e966d-6415-0410-94b9-dc4e65435d03>
2008-04-25 01:23:48 +0000

    IN PROGRESS - issue SJC-87: Port internal class processing to use Core Spring class reading API vs. standard reflection + Eliminated ConfigurationProcessor use within BeansAndPostProcessorTests

--- spring-javaconfig/branches/SJC-87/core/src/test/java/org/springframework/config/java/BeansAndPostprocessorTests.java
+++ spring-javaconfig/branches/SJC-87/core/src/test/java/org/springframework/config/java/BeansAndPostprocessorTests.java
@@ -27,81 +27,54 @@ import org.junit.Test;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.config.java.annotation.Bean;
-import org.springframework.config.java.annotation.Configuration;
-import org.springframework.config.java.context.JavaConfigApplicationContext;
-import org.springframework.config.java.process.ConfigurationProcessor;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.config.java.context.ConfigurableJavaConfigApplicationContext;
+import org.springframework.config.java.context.LegacyJavaConfigApplicationContext;
 import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
 import org.springframework.dao.support.PersistenceExceptionTranslationInterceptor;
 import org.springframework.stereotype.Repository;
 
 /**
  * @author Costin Leau
- *
+ * @author Chris Beams
  */
 public class BeansAndPostprocessorTests {
 
-	@Configuration
-	public static class MixedConfiguration {
-		@Bean
-		public Object publicBean() {
-			return "public bean";
-		}
+	private ConfigurableJavaConfigApplicationContext ctx;
+	private CountingBPP bpp;
 
-		@Bean
-		protected Object hiddenBean() {
-			return "hidden bean";
-		}
+
+	@Before
+	public void setUp() {
+		bpp = new CountingBPP();
+		ctx = new LegacyJavaConfigApplicationContext(MixedConfig.class) {
+			@Override
+			protected void customizeBeanFactory(DefaultListableBeanFactory beanFactory) {
+				super.customizeBeanFactory(beanFactory);
+				beanFactory.addBeanPostProcessor(bpp);
+			}
+		};
 	}
-
+	public static class MixedConfig {
+		public @Bean Object publicBean() { return "public bean"; }
+		protected @Bean Object hiddenBean() { return "hidden bean"; }
+	}
 	public static class CountingBPP implements BeanPostProcessor {
-
 		public Map<String, Object> beans = new LinkedHashMap<String, Object>();
-
 		public Object postProcessAfterInitialization(Object bean, String name) throws BeansException {
 			beans.put(name, bean);
 			return bean;
 		}
-
 		public Object postProcessBeforeInitialization(Object bean, String name) throws BeansException {
 			return bean;
 		}
-
 	}
 
-	public static class NewConfiguration {
-		@Bean
-		public Object newBean() {
-			return "new bean";
-		}
 
-		@Bean
-		public FooRepository repos() {
-			return new MyRepository();
-		}
+	@After
+	public void tearDown() { ctx.close(); }
 
-		@Bean
-		public PersistenceExceptionTranslationInterceptor intercept() {
-			return new PersistenceExceptionTranslationInterceptor();
-		}
-
-		@Bean
-		public PersistenceExceptionTranslationPostProcessor beanPostProcessor() {
-			PersistenceExceptionTranslationPostProcessor bpp = new PersistenceExceptionTranslationPostProcessor();
-			return bpp;
-		}
-
-	}
-
-	public static interface FooRepository {
-	}
-
-	@Repository
-	public static class MyRepository implements FooRepository {
-
-	}
 
 	/**
 	 * TODO: this test fails with an error stating that no persistence exception
@@ -112,45 +85,36 @@ public class BeansAndPostprocessorTests {
 	@Ignore
 	@Test
 	public void testViaApplicationContext() {
-		JavaConfigApplicationContext context = new JavaConfigApplicationContext(NewConfiguration.class);
+		LegacyJavaConfigApplicationContext context = new LegacyJavaConfigApplicationContext(NewConfiguration.class);
 		String name = "newBean";
 		context.getBean(Object.class, name);
 		CountingBPP postProcessor = context.getBean(CountingBPP.class);
 		assertTrue(postProcessor.beans.containsKey(name));
 	}
-
-	private ConfigurableApplicationContext ctx;
-
-	private ConfigurationProcessor configurationProcessor;
-
-	private CountingBPP bpp;
-
-	@Before
-	public void setUp() {
-		ctx = new GenericApplicationContext();
-		configurationProcessor = new ConfigurationProcessor(ctx);
-		configurationProcessor.afterPropertiesSet();
-
-		configurationProcessor.processClass(MixedConfiguration.class);
-
-		bpp = new CountingBPP();
-		ctx.getBeanFactory().addBeanPostProcessor(bpp);
-		ctx.refresh();
+	public static class NewConfiguration {
+		public @Bean Object newBean() { return "new bean"; }
+		public @Bean FooRepository repos() { return new MyRepository(); }
+		public @Bean PersistenceExceptionTranslationInterceptor intercept() {
+			return new PersistenceExceptionTranslationInterceptor();
+		}
+		public @Bean PersistenceExceptionTranslationPostProcessor beanPostProcessor() {
+			return new PersistenceExceptionTranslationPostProcessor();
+		}
 	}
+	public static interface FooRepository { }
+	public static @Repository class MyRepository implements FooRepository { }
 
-	@After
-	public void tearDown() {
-		ctx.close();
-	}
 
-	public void testBPPForPublicBeans() throws Exception {
+	public @Test void testBPPForPublicBeans() throws Exception {
 		String name = "publicBean";
 		Object bean = ctx.getBean(name);
 		assertNotNull(bean);
 		assertTrue(bpp.beans.containsKey(name));
 	}
 
-	public void testBPPForHiddenBeans() throws Exception {
+
+	// TODO: [hiding]
+	public @Test void testBPPForHiddenBeans() throws Exception {
 		String name = "hiddenBean";
 		try {
 			ctx.getBean(name);
