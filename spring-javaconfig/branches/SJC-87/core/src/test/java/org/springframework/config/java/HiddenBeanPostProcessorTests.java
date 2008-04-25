87d347f32ae88b02e6ecb65cf97cd303da21155d
cbeams <cbeams@2e8e966d-6415-0410-94b9-dc4e65435d03>
2008-04-25 03:24:04 +0000

    IN PROGRESS - issue SJC-87: Port internal class processing to use Core Spring class reading API vs. standard reflection + Eliminated ConfigurationProcessor use within three test classes

--- spring-javaconfig/branches/SJC-87/core/src/test/java/org/springframework/config/java/HiddenBeanPostProcessorTests.java
+++ spring-javaconfig/branches/SJC-87/core/src/test/java/org/springframework/config/java/HiddenBeanPostProcessorTests.java
@@ -15,6 +15,8 @@
  */
 package org.springframework.config.java;
 
+import static junit.framework.Assert.assertTrue;
+
 import java.awt.Point;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -32,40 +34,35 @@ import org.springframework.beans.factory.config.InstantiationAwareBeanPostProces
 import org.springframework.beans.factory.config.Scope;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.config.java.annotation.Bean;
-import org.springframework.config.java.annotation.Configuration;
-import org.springframework.config.java.process.ConfigurationProcessor;
+import org.springframework.config.java.context.JavaConfigApplicationContext;
+import org.springframework.config.java.context.LegacyJavaConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.context.support.GenericApplicationContext;
 
 /**
- * 
  * @author Rod Johnson
- * 
+ * @author Chris Beams
  */
 public class HiddenBeanPostProcessorTests {
 
-	@Configuration
-	public static class HasHiddenBeansNeedingPostProcessing {
+	public @Test void testBeanPostProcessorActsOnHiddenBeans() {
+		final RememberingBeanPostProcessor rememberingPostProcessor = new RememberingBeanPostProcessor();
 
-		@Bean
-		public Point publicPoint() {
-			return protectedPoint();
-		}
+		new JavaConfigApplicationContext(HasHiddenBeansNeedingPostProcessing.class) {
+			@Override
+			protected void customizeBeanFactory(DefaultListableBeanFactory beanFactory) {
+				super.customizeBeanFactory(beanFactory);
+				beanFactory.addBeanPostProcessor(rememberingPostProcessor);
+			}
+		}.getBean("publicPoint");
 
-		@Bean
-		protected Point protectedPoint() {
-			return packagePoint();
-		}
-
-		@Bean
-		Point packagePoint() {
-			return new Point();
-		}
-
+		assertTrue("Must have recorded public bean method",
+				rememberingPostProcessor.beansSeen.containsKey("publicPoint"));
+		assertTrue("Must have recorded package bean method",
+				rememberingPostProcessor.beansSeen.containsKey("packagePoint"));
+		assertTrue("Must have recorded protected bean method",
+				rememberingPostProcessor.beansSeen.containsKey("protectedPoint"));
 	}
-
 	public static class RememberingBeanPostProcessor extends InstantiationAwareBeanPostProcessorAdapter {
-
 		private Map<String, Class<?>> beansSeen = new HashMap<String, Class<?>>();
 
 		@Override
@@ -78,28 +75,38 @@ public class HiddenBeanPostProcessorTests {
 		}
 	}
 
-	@Test
-	public void testBeanPostProcessorActsOnHiddenBeans() {
-		DefaultListableBeanFactory bf = new DefaultListableBeanFactory();
-		RememberingBeanPostProcessor rememberingPostProcessor = new RememberingBeanPostProcessor();
-		bf.addBeanPostProcessor(rememberingPostProcessor);
-		ConfigurationProcessor configurationProcessor = new ConfigurationProcessor(bf);
 
-		configurationProcessor.processClass(HasHiddenBeansNeedingPostProcessing.class);
+	public @Test void testBeanFactoryPostProcessorActsOnHiddenBeans() {
+		RememberingBeanFactoryPostProcessor rememberingFactoryPostProcessor = new RememberingBeanFactoryPostProcessor();
 
-		// Will get others
-		bf.getBean("publicPoint");
+		JavaConfigApplicationContext ctx = new JavaConfigApplicationContext();
+		ctx.addBeanFactoryPostProcessor(rememberingFactoryPostProcessor);
+		ctx.addConfigClasses(HasHiddenBeansNeedingPostProcessing.class);
+		ctx.refresh();
 
-		Assert.assertTrue("Must have recorded public bean method", rememberingPostProcessor.beansSeen
-				.containsKey("publicPoint"));
-		Assert.assertTrue("Must have recorded package bean method", rememberingPostProcessor.beansSeen
-				.containsKey("packagePoint"));
-		Assert.assertTrue("Must have recorded protected bean method", rememberingPostProcessor.beansSeen
-				.containsKey("protectedPoint"));
+		validateBeanFactoryPostProcessor(rememberingFactoryPostProcessor);
+	}
+	public static class HasHiddenBeansNeedingPostProcessing {
+		public @Bean Point publicPoint() { return protectedPoint(); }
+		protected @Bean Point protectedPoint() { return packagePoint(); }
+		@Bean Point packagePoint() { return new Point(); }
 	}
 
-	public static class RememberingBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
 
+	public @Test void testApplicationContextWithXml() {
+		ClassPathXmlApplicationContext cac = new ClassPathXmlApplicationContext(
+				new String[] { "/org/springframework/config/java/hiddenBeanPostProcessorTests.xml" }, true);
+
+		RememberingBeanFactoryPostProcessor rememberingFactoryPostProcessor =
+			(RememberingBeanFactoryPostProcessor) cac.getBean("bfpp1");
+		validateBeanFactoryPostProcessor(rememberingFactoryPostProcessor);
+
+		rememberingFactoryPostProcessor = (RememberingBeanFactoryPostProcessor) cac.getBean("bfpp2");
+		validateBeanFactoryPostProcessor(rememberingFactoryPostProcessor);
+	}
+
+
+	private static class RememberingBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
 		private Set<String> beansSeen = new HashSet<String>();
 
 		public void postProcessBeanFactory(ConfigurableListableBeanFactory clbf) throws BeansException {
@@ -113,32 +120,6 @@ public class HiddenBeanPostProcessorTests {
 		}
 	}
 
-	@Test
-	public void testBeanFactoryPostProcessorActsOnHiddenBeans() {
-		// BeanFactoryPostProcessors work only on ApplicationContext
-		GenericApplicationContext gac = new GenericApplicationContext();
-
-		RememberingBeanFactoryPostProcessor rememberingFactoryPostProcessor = new RememberingBeanFactoryPostProcessor();
-		gac.addBeanFactoryPostProcessor(rememberingFactoryPostProcessor);
-		ConfigurationProcessor configurationProcessor = new ConfigurationProcessor(gac);
-		configurationProcessor.processClass(HasHiddenBeansNeedingPostProcessing.class);
-
-		gac.refresh();
-
-		validateBeanFactoryPostProcessor(rememberingFactoryPostProcessor);
-	}
-
-	@Test
-	public void testApplicationContextWithXml() {
-		ClassPathXmlApplicationContext cac = new ClassPathXmlApplicationContext(
-				new String[] { "/org/springframework/config/java/hiddenBeanPostProcessorTests.xml" }, true);
-
-		RememberingBeanFactoryPostProcessor rememberingFactoryPostProcessor = (RememberingBeanFactoryPostProcessor) cac
-				.getBean("bfpp1");
-		validateBeanFactoryPostProcessor(rememberingFactoryPostProcessor);
-		rememberingFactoryPostProcessor = (RememberingBeanFactoryPostProcessor) cac.getBean("bfpp2");
-		validateBeanFactoryPostProcessor(rememberingFactoryPostProcessor);
-	}
 
 	private void validateBeanFactoryPostProcessor(RememberingBeanFactoryPostProcessor rememberingFactoryPostProcessor) {
 		Assert.assertTrue("Must have recorded public bean method", rememberingFactoryPostProcessor.beansSeen
@@ -149,41 +130,28 @@ public class HiddenBeanPostProcessorTests {
 				.contains("protectedPoint"));
 	}
 
-	@Configuration
-	static class ScopedContext {
-		@Bean(scope = "fooScope")
-		public Object foo() {
-			return new Object();
-		}
-	}
 
+	// TODO: [bean scoping]
+	public @Test void testScopesCopiedFromParent() {
+		new LegacyJavaConfigApplicationContext(ScopedContextConfig.class) {
+			@Override
+			protected void customizeBeanFactory(DefaultListableBeanFactory beanFactory) {
+				super.customizeBeanFactory(beanFactory);
+				beanFactory.registerScope("fooScope", new DummyScope());
+			}
+		}.getBean("foo");
+	}
+	static class ScopedContextConfig {
+		@Bean(scope = "fooScope")
+		public Object foo() { return new Object(); }
+	}
 	// Note: This class would not work as a scope
 	// We are only trying to check Scope registration, not functionality
 	public class DummyScope implements Scope {
-
-		public Object get(String s, ObjectFactory of) {
-			return null;
-		}
-
-		public String getConversationId() {
-			return null;
-		}
-
-		public void registerDestructionCallback(String name, Runnable r) {
-		}
-
-		public Object remove(String name) {
-			return null;
-		}
+		public Object get(String s, ObjectFactory of) { return null; }
+		public String getConversationId() { return null; }
+		public void registerDestructionCallback(String name, Runnable r) { }
+		public Object remove(String name) { return null; }
 	}
 
-	@Test
-	public void testScopesCopiedFromParent() {
-		DefaultListableBeanFactory bf = new DefaultListableBeanFactory();
-		bf.registerScope("fooScope", new DummyScope());
-		ConfigurationProcessor configurationProcessor = new ConfigurationProcessor(bf);
-
-		configurationProcessor.processClass(ScopedContext.class);
-		bf.getBean("foo");
-	}
 }
