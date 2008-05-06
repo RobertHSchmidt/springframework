1438fa87c5060c92aa7ffd05d207c12a8c5eb143
nebhale <nebhale@2e8e966d-6415-0410-94b9-dc4e65435d03>
2008-05-06 15:51:54 +0000

    [maven-release-plugin]  copy for tag spring-batch-1.0.1.RELEASE

---
+++ spring-batch/tags/spring-batch-1.0.1.RELEASE/spring-batch-infrastructure/src/main/java/org/springframework/batch/repeat/support/ResultHolder.java
@@ -0,0 +1,36 @@
+package org.springframework.batch.repeat.support;
+
+import org.springframework.batch.repeat.ExitStatus;
+import org.springframework.batch.repeat.RepeatContext;
+
+/**
+ * Interface for result holder. 
+ * 
+ * @author Dave Syer
+ */
+interface ResultHolder {
+	/**
+	 * Get the result for client from this holder. Does not block if none is
+	 * available yet.
+	 * 
+	 * @return the result, or null if there is none.
+	 * @throws IllegalStateException
+	 */
+	ExitStatus getResult();
+
+	/**
+	 * Get the error for client from this holder if any. Does not block if
+	 * none is available yet.
+	 * 
+	 * @return the error, or null if there is none.
+	 * @throws IllegalStateException
+	 */
+	Throwable getError();
+
+	/**
+	 * Get the context in which the result evaluation is executing.
+	 * 
+	 * @return the context of the result evaluation.
+	 */
+	RepeatContext getContext();
+}
---
+++ spring-batch/tags/spring-batch-1.0.1.RELEASE/spring-batch-samples/src/test/java/org/springframework/batch/sample/IbatisJobFunctionalTests.java
@@ -0,0 +1,11 @@
+package org.springframework.batch.sample;
+
+
+/**
+ * Test for IbatisJob - checks that customer credit has been updated to expected value.
+ * 
+ * @author Robert Kasanicky
+ */
+public class IbatisJobFunctionalTests extends AbstractCustomerCreditIncreaseTests{
+
+}
