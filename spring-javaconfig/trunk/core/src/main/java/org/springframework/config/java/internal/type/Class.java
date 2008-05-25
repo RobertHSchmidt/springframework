47f039219d9e91ba097f149395fc91fe67027903
cbeams <cbeams@2e8e966d-6415-0410-94b9-dc4e65435d03>
2008-05-25 01:32:20 +0000

    IN PROGRESS - issue SJC-99: Repackage new internals, ensuring no cycles exist

---
+++ spring-javaconfig/trunk/core/src/main/java/org/springframework/config/java/internal/type/Class.java
@@ -0,0 +1,42 @@
+package org.springframework.config.java.internal.type;
+
+import org.springframework.util.Assert;
+
+public class Class {
+
+	private final String name;
+	private String pkg;
+
+	public Class(String name) {
+		this.name = name;
+	}
+
+	public Class setPackage(String pkg) {
+		this.pkg = pkg;
+		return this;
+	}
+
+	public String getPackage() {
+		return this.pkg;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * Create a new ConfigurationClass for a given {@link java.lang.Class}.
+	 * A very limited subset of data is populated for the class, just class name
+	 * and package name.
+	 */
+	public static Class forClass(java.lang.Class<?> clazz) {
+		return new Class(clazz.getSimpleName()).setPackage(clazz.getPackage().getName());
+	}
+
+	public String getFullyQualifiedName() {
+		Assert.notNull("package must be non-null", getPackage());
+		return getPackage().concat(".").concat(getName());
+	}
+
+
+}
