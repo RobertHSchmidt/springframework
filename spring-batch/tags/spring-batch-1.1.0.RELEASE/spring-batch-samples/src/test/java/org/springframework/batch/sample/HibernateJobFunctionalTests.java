81af77b685c58662732e4d6323e219ecf47525af
macsuibhne <macsuibhne@2e8e966d-6415-0410-94b9-dc4e65435d03>
2008-07-18 12:09:12 +0000

    spring-batch-1.1.0.RELEASE

---
+++ spring-batch/tags/spring-batch-1.1.0.RELEASE/spring-batch-samples/src/test/java/org/springframework/batch/sample/HibernateJobFunctionalTests.java
@@ -0,0 +1,10 @@
+package org.springframework.batch.sample;
+
+/**
+ * Test for HibernateJob - checks that customer credit has been updated to expected value.
+ * 
+ * @author Robert Kasanicky
+ */
+public class HibernateJobFunctionalTests extends AbstractCustomerCreditIncreaseTests {
+
+}
